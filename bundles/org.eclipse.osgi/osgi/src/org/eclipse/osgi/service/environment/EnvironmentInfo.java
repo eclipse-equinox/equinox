1d53858c430e00bf0fd023f1a13ac0d226b09644
DJ Houghton <dj>
2003-11-26 18:59:22 +0000

    Added copyright.

--- bundles/org.eclipse.osgi/osgi/src/org/eclipse/osgi/service/environment/EnvironmentInfo.java
+++ bundles/org.eclipse.osgi/osgi/src/org/eclipse/osgi/service/environment/EnvironmentInfo.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2003 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.osgi.service.environment;
 
 //TODO There is basically two kinds of info that are required from the running OSGi : some are related to the args that have been passed to the system,
