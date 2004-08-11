f40b296bb3e38c7d43db5aefdb3d856ec9c4b9c5
DJ Houghton <dj>
2004-08-11 14:31:55 +0000

    Bug 70436 [osgi] Infinite loop in basicStopBundles with IBM JRE 1.4.1 SR2

--- bundles/org.eclipse.osgi/eclipseAdaptor/src/org/eclipse/core/runtime/adaptor/BundleStopper.java
+++ bundles/org.eclipse.osgi/eclipseAdaptor/src/org/eclipse/core/runtime/adaptor/BundleStopper.java
@@ -67,18 +67,17 @@ public class BundleStopper {
 		// stop all active bundles in the reverse order of Require-Bundle
 		for (int stoppingIndex = allToStop.length - 1; stoppingIndex >= 0; stoppingIndex--) {
 			AbstractBundle toStop = (AbstractBundle) context.getBundle(allToStop[stoppingIndex].getBundleId());
-			if (toStop.getBundleId() == 0 || !((EclipseBundleData) toStop.getBundleData()).isAutoStartable())
-				continue;
-			try {
-				if (toStop.getState() != Bundle.ACTIVE || !(toStop instanceof BundleHost))
-					continue;
-				toStop.stop();
-			} catch (Exception e) {
-				String message = EclipseAdaptorMsg.formatter.getString("ECLIPSE_BUNDLESTOPPER_ERROR_STOPPING_BUNDLE", allToStop[stoppingIndex].toString()); //$NON-NLS-1$
-				FrameworkLogEntry entry = new FrameworkLogEntry(FrameworkAdaptor.FRAMEWORK_SYMBOLICNAME, message, 0, e, null);
-				EclipseAdaptor.getDefault().getFrameworkLog().log(entry);
-			} finally {
-				stoppedBundles.put(toStop, toStop);
+			if (toStop.getBundleId() != 0 && ((EclipseBundleData) toStop.getBundleData()).isAutoStartable()) {
+				try {
+					if ((toStop.getState() == Bundle.ACTIVE) && (toStop instanceof BundleHost))
+						toStop.stop();
+				} catch (Exception e) {
+					String message = EclipseAdaptorMsg.formatter.getString("ECLIPSE_BUNDLESTOPPER_ERROR_STOPPING_BUNDLE", allToStop[stoppingIndex].toString()); //$NON-NLS-1$
+					FrameworkLogEntry entry = new FrameworkLogEntry(FrameworkAdaptor.FRAMEWORK_SYMBOLICNAME, message, 0, e, null);
+					EclipseAdaptor.getDefault().getFrameworkLog().log(entry);
+				} finally {
+					stoppedBundles.put(toStop, toStop);
+				}
 			}
 		}
 	}
