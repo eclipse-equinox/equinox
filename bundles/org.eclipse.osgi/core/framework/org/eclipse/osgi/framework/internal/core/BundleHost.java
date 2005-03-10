f93be91304f7a13df314feac71d4d16ec9df3012
Thomas Watson <twatson>
2005-03-10 14:41:34 +0000

    Bug 87593    BundleHost.load() uses null framework.permissionAdmin because it assumes the field was initialized during framework initialization

--- bundles/org.eclipse.osgi/core/framework/org/eclipse/osgi/framework/internal/core/BundleHost.java
+++ bundles/org.eclipse.osgi/core/framework/org/eclipse/osgi/framework/internal/core/BundleHost.java
@@ -59,7 +59,7 @@ public class BundleHost extends AbstractBundle {
 		if (framework.isActive()) {
 			SecurityManager sm = System.getSecurityManager();
 
-			if (sm != null) {
+			if (sm != null && framework.permissionAdmin != null) {
 				domain = framework.permissionAdmin.createProtectionDomain(this);
 			}
 
@@ -109,7 +109,7 @@ public class BundleHost extends AbstractBundle {
 		this.bundledata = newBundle.bundledata;
 		this.bundledata.setBundle(this);
 		// create a new domain for the bundle because its signers/symbolic-name may have changed
-		if (framework.isActive() && System.getSecurityManager() != null)
+		if (framework.isActive() && System.getSecurityManager() != null && framework.permissionAdmin != null)
 			domain = framework.permissionAdmin.createProtectionDomain(this);
 		return (exporting);
 	}
