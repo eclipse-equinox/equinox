1892523a38c12be31a38e6d7047880161de2b97d
Thomas Watson <twatson>
2005-04-04 16:57:30 +0000

    Bug 90080    ContentHandlerFactory conflicts with SimpleBeanInfo.loadImage()

--- bundles/org.eclipse.osgi/core/framework/org/eclipse/osgi/framework/internal/protocol/StreamHandlerFactory.java
+++ bundles/org.eclipse.osgi/core/framework/org/eclipse/osgi/framework/internal/protocol/StreamHandlerFactory.java
@@ -34,6 +34,7 @@ public class StreamHandlerFactory implements java.net.URLStreamHandlerFactory {
 	protected static final String URLSTREAMHANDLERCLASS = "org.osgi.service.url.URLStreamHandlerService"; //$NON-NLS-1$
 	protected static final String PROTOCOL_HANDLER_PKGS= "java.protocol.handler.pkgs"; //$NON-NLS-1$
 	protected static final String INTERNAL_PROTOCOL_HANDLER_PKG = "org.eclipse.osgi.framework.internal.protocol."; //$NON-NLS-1$
+	private static final String DEFAULT_VM_PROTOCOL_HANDLERS = "sun.net.www.protocol"; //$NON-NLS-1$
 
 	private Hashtable proxies;
 
@@ -63,6 +64,7 @@ public class StreamHandlerFactory implements java.net.URLStreamHandlerFactory {
 
 		//first check for built in handlers
 		String builtInHandlers = System.getProperty(PROTOCOL_HANDLER_PKGS);
+		builtInHandlers = builtInHandlers == null ? DEFAULT_VM_PROTOCOL_HANDLERS : DEFAULT_VM_PROTOCOL_HANDLERS + '|' + builtInHandlers;
 		Class clazz = null;
 		if (builtInHandlers != null) {
 			StringTokenizer tok = new StringTokenizer(builtInHandlers, "|"); //$NON-NLS-1$
