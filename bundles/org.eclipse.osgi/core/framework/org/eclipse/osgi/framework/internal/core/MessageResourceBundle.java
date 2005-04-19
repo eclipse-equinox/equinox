bddcb17389057316289456397de2f41cbf6b65ce
John Arthorne <johna>
2005-04-19 20:38:33 +0000

    Bug 90526 [osgi] Need to log exceptions in MessageResourceBundle

--- bundles/org.eclipse.osgi/core/framework/org/eclipse/osgi/framework/internal/core/MessageResourceBundle.java
+++ bundles/org.eclipse.osgi/core/framework/org/eclipse/osgi/framework/internal/core/MessageResourceBundle.java
@@ -17,7 +17,9 @@ import java.lang.reflect.Modifier;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
 import java.util.*;
+import org.eclipse.osgi.framework.adaptor.FrameworkAdaptor;
 import org.eclipse.osgi.framework.debug.Debug;
+import org.eclipse.osgi.framework.log.FrameworkLogEntry;
 
 /**
  * Class responsible for loading message values from a property file
@@ -48,6 +50,23 @@ public class MessageResourceBundle {
 			this.isAccessible = isAccessible;
 		}
 
+		/*
+		 * Change the accessibility of the specified field so we can set its value
+		 * to be the appropriate message string.
+		 */
+		private void makeAccessible(final Field field) {
+			if (System.getSecurityManager() == null) {
+				field.setAccessible(true);
+			} else {
+				AccessController.doPrivileged(new PrivilegedAction() {
+					public Object run() {
+						field.setAccessible(true);
+						return null;
+					}
+				});
+			}
+		}
+
 		/* (non-Javadoc)
 		 * @see java.util.Hashtable#put(java.lang.Object, java.lang.Object)
 		 */
@@ -57,8 +76,9 @@ public class MessageResourceBundle {
 			if (fieldObject == ASSIGNED)
 				return null;
 			if (fieldObject == null) {
-				if (DEBUG_MESSAGE_BUNDLES)
+				if (Debug.DEBUG_MESSAGE_BUNDLES)
 					System.out.println("Unused message: " + key + " in: " + bundleName); //$NON-NLS-1$ //$NON-NLS-2$
+				log(SEVERITY_WARNING, "Warning unused message: " + key + " in: " + bundleName, null); //$NON-NLS-1$ //$NON-NLS-2$
 				return null;
 			}
 			final Field field = (Field) fieldObject;
@@ -77,42 +97,26 @@ public class MessageResourceBundle {
 				// will fail later in the code and if so then we will see both the NPE and this error.
 				field.set(null, value);
 			} catch (Exception e) {
-				// TODO externalize message
-				//IStatus status = new Status(IStatus.ERROR, Platform.PI_RUNTIME, Platform.PLUGIN_ERROR, "Exception setting field value.", e);
-				//InternalPlatform.getDefault().log(status);
-				e.printStackTrace();
+				log(SEVERITY_ERROR, "Exception setting field value.", e); //$NON-NLS-1$
 			}
 			return null;
 		}
-
-		/*
-		 * Change the accessibility of the specified field so we can set its value
-		 * to be the appropriate message string.
-		 */
-		private void makeAccessible(final Field field) {
-			if (System.getSecurityManager() == null) {
-				field.setAccessible(true);
-			} else {
-				AccessController.doPrivileged(new PrivilegedAction() {
-					public Object run() {
-						field.setAccessible(true);
-						return null;
-					}
-				});
-			}
-		}
 	}
 
+	private static FrameworkAdaptor adaptor;
 	/**
 	 * This object is assigned to the value of a field map to indicate
 	 * that a translated message has already been assigned to that field.
 	 */
 	static final Object ASSIGNED = new Object();
 
-	static final boolean DEBUG_MESSAGE_BUNDLES = Debug.DEBUG_MESSAGE_BUNDLES;
-
 	private static final String EXTENSION = ".properties"; //$NON-NLS-1$
+
 	private static String[] nlSuffixes;
+
+	static int SEVERITY_ERROR = 0x04;
+
+	static int SEVERITY_WARNING = 0x02;
 
 	/*
 	 * Build an array of property files to search.  The returned array contains
@@ -161,16 +165,17 @@ public class MessageResourceBundle {
 				// we know we have a public static non-final field. If we do get an exception, silently
 				// log it and continue. This means that the field will (most likely) be un-initialized and
 				// will fail later in the code and if so then we will see both the NPE and this error.
-				String value = "Missing message: " + field.getName() + " in: " + bundleName; //$NON-NLS-1$ //$NON-NLS-2$
-				if (DEBUG_MESSAGE_BUNDLES)
+				String value = "Warning missing message: " + field.getName() + " in: " + bundleName; //$NON-NLS-1$ //$NON-NLS-2$
+				if (Debug.DEBUG_MESSAGE_BUNDLES)
 					System.out.println(value);
+				log(SEVERITY_WARNING, value, null);
 				field.set(null, value);
 			} catch (IllegalArgumentException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				// TODO externalize message
+				log(SEVERITY_ERROR, "Error setting the missing message value.", e); //$NON-NLS-1$
 			} catch (IllegalAccessException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				// TODO externalize message
+				log(SEVERITY_ERROR, "Error setting the missing message value.", e); //$NON-NLS-1$
 			}
 		}
 	}
@@ -203,7 +208,7 @@ public class MessageResourceBundle {
 				final MessagesProperties properties = new MessagesProperties(fields, bundleName, isAccessible);
 				properties.load(input);
 			} catch (IOException e) {
-				// TODO log
+				log(SEVERITY_ERROR, "Error loading " + variants[i], e); //$NON-NLS-1$
 			} finally {
 				if (input != null)
 					try {
@@ -214,7 +219,21 @@ public class MessageResourceBundle {
 			}
 		}
 		computeMissingMessages(bundleName, clazz, fields, fieldArray);
-		if (DEBUG_MESSAGE_BUNDLES)
+		if (Debug.DEBUG_MESSAGE_BUNDLES)
 			System.out.println("Time to load message bundle: " + bundleName + " was " + (System.currentTimeMillis() - start) + "ms."); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+	}
+
+	static void log(int severity, String msg, Exception e) {
+		if (adaptor != null)
+			adaptor.getFrameworkLog().log(new FrameworkLogEntry(FrameworkAdaptor.FRAMEWORK_SYMBOLICNAME + ' ' + severity + ' ' + 1, msg, 0, e, null));
+		else {
+			System.out.println(msg); //$NON-NLS-1$
+			if (e != null)
+				e.printStackTrace();
+		}
+	}
+
+	static void setAdaptor(FrameworkAdaptor adaptor) {
+		MessageResourceBundle.adaptor = adaptor;
 	}
 }
