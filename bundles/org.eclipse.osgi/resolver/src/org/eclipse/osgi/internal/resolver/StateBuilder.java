db4cd7f6ab7b35206274de63cfd9382ae98d4e5a
Thomas Watson <twatson>
2005-05-10 21:39:58 +0000

    backout changed for bug 87780

--- bundles/org.eclipse.osgi/resolver/src/org/eclipse/osgi/internal/resolver/StateBuilder.java
+++ bundles/org.eclipse.osgi/resolver/src/org/eclipse/osgi/internal/resolver/StateBuilder.java
@@ -69,34 +69,17 @@ class StateBuilder {
 		ManifestElement[] dynamicImports = ManifestElement.parseHeader(Constants.DYNAMICIMPORT_PACKAGE, (String) manifest.get(Constants.DYNAMICIMPORT_PACKAGE));
 		result.setImportPackages(createImportPackages(result.getExportPackages(), providedExports, imports, dynamicImports, manifestVersion));
 		ManifestElement[] requires = ManifestElement.parseHeader(Constants.REQUIRE_BUNDLE, (String) manifest.get(Constants.REQUIRE_BUNDLE));
-		// make sure osgi is required if: not a fragment, manifestVersion < 2, not the system bundle, and the system bundle is available
-		boolean forceRequireOSGi = state != null && host == null && manifestVersion < 2 && Constants.getInternalSymbolicName() != null && !Constants.getInternalSymbolicName().equals(result.getSymbolicName()) && state.getBundle(Constants.getInternalSymbolicName(), null) != null;
-		result.setRequiredBundles(createRequiredBundles(requires, forceRequireOSGi));
+		result.setRequiredBundles(createRequiredBundles(requires));
 		return result;
 	}
 
-	private static BundleSpecification[] createRequiredBundles(ManifestElement[] specs, boolean forceRequireOSGi) {
-		if (specs == null) {
-			if (forceRequireOSGi)
-				return new BundleSpecification[] {createRequireSystemBundle()};
+	private static BundleSpecification[] createRequiredBundles(ManifestElement[] specs) {
+		if (specs == null)
 			return null;
-		}
-		ArrayList result = new ArrayList(specs.length);
-		boolean osgiRequired = false;
-		for (int i = 0; i < specs.length; i++) {
-			if (Constants.getInternalSymbolicName().equals(specs[i].getValue()))
-				osgiRequired = true;
-			result.add(createRequiredBundle(specs[i]));
-		}
-		if (forceRequireOSGi && !osgiRequired)
-			result.add(0, createRequireSystemBundle());
-		return (BundleSpecification[]) result.toArray(new BundleSpecification[result.size()]);
-	}
-
-	private static BundleSpecification createRequireSystemBundle() {
-		BundleSpecificationImpl systemBundle = new BundleSpecificationImpl();
-		systemBundle.setName(Constants.getInternalSymbolicName());
-		return systemBundle;
+		BundleSpecification[] result = new BundleSpecification[specs.length];
+		for (int i = 0; i < specs.length; i++)
+			result[i] = createRequiredBundle(specs[i]);
+		return result;
 	}
 
 	private static BundleSpecification createRequiredBundle(ManifestElement spec) {
