48e5b984f997c9ef387f48350a0abe014eb775e8
Thomas Watson <twatson>
2005-09-28 18:47:24 +0000

    Revert back to not calling BundleActivator constructor in a doPriv.

--- bundles/org.eclipse.osgi/core/framework/org/eclipse/osgi/framework/internal/core/AbstractBundle.java
+++ bundles/org.eclipse.osgi/core/framework/org/eclipse/osgi/framework/internal/core/AbstractBundle.java
@@ -130,20 +130,13 @@ public abstract class AbstractBundle implements Bundle, Comparable, KeyedElement
 		String activatorClassName = bundledata.getActivator();
 		if (activatorClassName != null) {
 			try {
-				final Class activatorClass = loadClass(activatorClassName, false);
+				Class activatorClass = loadClass(activatorClassName, false);
 				/* Create the activator for the bundle */
-				if (System.getSecurityManager() == null)
-					return (BundleActivator) activatorClass.newInstance();
-				return (BundleActivator) AccessController.doPrivileged(new PrivilegedExceptionAction() {
-					public Object run() throws Exception {
-						return activatorClass.newInstance();
-					}
-				});
+				return (BundleActivator) (activatorClass.newInstance());
 			} catch (Throwable t) {
-				if (t instanceof PrivilegedActionException)
-					t = ((PrivilegedActionException)t).getException();
-				if (Debug.DEBUG && Debug.DEBUG_GENERAL)
+				if (Debug.DEBUG && Debug.DEBUG_GENERAL) {
 					Debug.printStackTrace(t);
+				}
 				throw new BundleException(NLS.bind(Msg.BUNDLE_INVALID_ACTIVATOR_EXCEPTION, activatorClassName, bundledata.getSymbolicName()), t);
 			}
 		}
