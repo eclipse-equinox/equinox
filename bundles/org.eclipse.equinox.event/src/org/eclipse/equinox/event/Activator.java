50135199bb7070c4a14a6ec54f29b3e2191e062a
jfogell <>
2006-02-06 17:05:23 +0000

    fix for #125835 - remove unused variable

--- bundles/org.eclipse.equinox.event/src/org/eclipse/equinox/event/Activator.java
+++ bundles/org.eclipse.equinox.event/src/org/eclipse/equinox/event/Activator.java
@@ -5,15 +5,13 @@ import org.eclipsei.equinox.event.mapper.EventRedeliverer;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
-import org.osgi.service.event.EventAdmin;
 
 public class Activator implements BundleActivator {
 	private EventRedeliverer    _eventRedeliverer= null;
-	private EventAdmin    	    _eventAdminImpl  = null;
 	private ServiceRegistration _eventAdminService = null;
 	public void start(BundleContext bundleContext) {
 		
-		_eventAdminService = bundleContext.registerService("org.osgi.service.event.EventAdmin",
+		_eventAdminService = bundleContext.registerService("org.osgi.service.event.EventAdmin", //$NON-NLS-1$
 				new EventAdminImpl(bundleContext),null);
 		_eventRedeliverer  = new EventRedeliverer(bundleContext);
 		_eventRedeliverer.open();
