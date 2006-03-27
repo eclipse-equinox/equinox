5acff2ea01cf02c3d32e9929f71c67b087e8e598
DJ Houghton <dj>
2006-03-27 15:25:55 +0000

    Bug 132666 - Remove product preference customization interface from the API

--- bundles/org.eclipse.equinox.preferences/src/org/eclipse/core/internal/preferences/IProductPreferencesService.java
+++ bundles/org.eclipse.equinox.preferences/src/org/eclipse/core/internal/preferences/IProductPreferencesService.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005 IBM Corporation and others.
+ * Copyright (c) 2005, 2006 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,14 +8,14 @@
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
-package org.eclipse.core.runtime.preferences;
+package org.eclipse.core.internal.preferences;
 
 import java.util.Properties;
 
 /**
  * A product can customize preferences by implementing this service.
  * 
- * @since org.eclipse.equinox.preferences 1.0
+ * @since org.eclipse.equinox.preferences 3.2
  */
 public interface IProductPreferencesService {
 	/**
