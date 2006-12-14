79f3f1347b1f085ecf0c6820192ccf1b5fb63b2d
Andrew Niefer <aniefer>
2006-12-14 19:10:11 +0000

    fix package name

--- bundles/org.eclipse.equinox.launcher/src/org/eclipse/equinox/launcher/WebStartMain.java
+++ bundles/org.eclipse.equinox.launcher/src/org/eclipse/equinox/launcher/WebStartMain.java
@@ -8,7 +8,7 @@
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
-package org.eclipse.core.launcher;
+package org.eclipse.equinox.launcher;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
