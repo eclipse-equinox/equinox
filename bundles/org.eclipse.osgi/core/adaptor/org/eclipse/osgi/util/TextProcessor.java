2184f0ca40ec71a3f02550ad9d578255db3e4bcb
Thomas Watson <twatson>
2007-03-30 19:55:19 +0000

    Bug 179321 TextProcessor should treat "he" as a rtl language

--- bundles/org.eclipse.osgi/core/adaptor/org/eclipse/osgi/util/TextProcessor.java
+++ bundles/org.eclipse.osgi/core/adaptor/org/eclipse/osgi/util/TextProcessor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006 IBM Corporation and others.
+ * Copyright (c) 2006, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -78,8 +78,7 @@ public class TextProcessor {
 		Locale locale = Locale.getDefault();
 		String lang = locale.getLanguage();
 
-		if ("iw".equals(lang) || "ar".equals(lang) || "fa".equals(lang) //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-				|| "ur".equals(lang)) //$NON-NLS-1$
+		if ("iw".equals(lang) || "he".equals(lang) || "ar".equals(lang) || "fa".equals(lang) || "ur".equals(lang)) //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$
 			isBidi = true;
 		
 		String osName = System.getProperty("os.name").toLowerCase(); //$NON-NLS-1$
