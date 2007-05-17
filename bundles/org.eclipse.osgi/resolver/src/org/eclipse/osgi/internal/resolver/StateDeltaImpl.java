3014737f1d6fa5948f2cf82ceed07438b1c54bfd
Thomas Watson <twatson>
2007-05-17 20:16:45 +0000

    Bug 187616 StateDeltaImpl.getChanges does not conform to the API

--- bundles/org.eclipse.osgi/resolver/src/org/eclipse/osgi/internal/resolver/StateDeltaImpl.java
+++ bundles/org.eclipse.osgi/resolver/src/org/eclipse/osgi/internal/resolver/StateDeltaImpl.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2003, 2006 IBM Corporation and others.
+ * Copyright (c) 2003, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -30,7 +30,7 @@ public class StateDeltaImpl implements StateDelta {
 		List result = new ArrayList();
 		for (Iterator changesIter = changes.values().iterator(); changesIter.hasNext();) {
 			BundleDelta change = (BundleDelta) changesIter.next();
-			if (mask == change.getType() || (!exact && (change.getType() & mask) == mask))
+			if (mask == change.getType() || (!exact && (change.getType() & mask) != 0))
 				result.add(change);
 		}
 		return (BundleDelta[]) result.toArray(new BundleDelta[result.size()]);
