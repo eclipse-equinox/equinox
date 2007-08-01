203161985f44e48599fc34d878dfdf63d152ebe5
Thomas Watson <twatson>
2007-08-01 15:32:48 +0000

    Bug 198535 HostSpecification#getHosts() returns null

--- bundles/org.eclipse.osgi/resolver/src/org/eclipse/osgi/internal/resolver/HostSpecificationImpl.java
+++ bundles/org.eclipse.osgi/resolver/src/org/eclipse/osgi/internal/resolver/HostSpecificationImpl.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2003, 2005 IBM Corporation and others.
+ * Copyright (c) 2003, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -30,7 +30,7 @@ public class HostSpecificationImpl extends VersionConstraintImpl implements Host
 	}
 
 	public BundleDescription[] getHosts() {
-		return hosts;
+		return hosts == null ? BundleDescriptionImpl.EMPTY_BUNDLEDESCS : hosts;
 	}
 
 	public boolean isResolved() {
