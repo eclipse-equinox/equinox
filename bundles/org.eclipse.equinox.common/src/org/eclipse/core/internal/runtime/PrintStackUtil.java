2fd50d695efdfb21c425e0c0c60911fbd8e27f16
Oleg Besedin <obesedin>
2008-04-02 15:21:05 +0000

    Bug 158123 Improve reporting of CoreException(MultiStatus)

--- bundles/org.eclipse.equinox.common/src/org/eclipse/core/internal/runtime/PrintStackUtil.java
+++ bundles/org.eclipse.equinox.common/src/org/eclipse/core/internal/runtime/PrintStackUtil.java
@@ -14,55 +14,32 @@ import java.io.PrintStream;
 import java.io.PrintWriter;
 import org.eclipse.core.runtime.IStatus;
 
-/**
- * There are entry methods for both PrintStream and PrintWriter. The PrintStream 
- * access point does flush() on the derived PrintWriter after text has been 
- * printed out.
- */
 public class PrintStackUtil {
 
 	static public void printChildren(IStatus status, PrintStream output) {
-		Throwable cause = status.getException();
-		StackTraceElement[] topStack = (cause == null) ? null : cause.getStackTrace();
-
-		PrintWriter writer = new PrintWriter(output);
-		printChildren(status, writer, topStack, 1);
-		writer.flush();
+		IStatus[] children = status.getChildren();
+		if (children == null || children.length == 0)
+			return;
+		for (int i = 0; i < children.length; i++) {
+			output.println("Contains: " + children[i].getMessage()); //$NON-NLS-1$
+			Throwable exception = children[i].getException();
+			if (exception != null)
+				exception.printStackTrace();
+			printChildren(children[i], output);
+		}
 	}
 
 	static public void printChildren(IStatus status, PrintWriter output) {
-		Throwable cause = status.getException();
-		StackTraceElement[] topStack = (cause == null) ? null : cause.getStackTrace();
-		printChildren(status, output, topStack, 1);
-	}
-
-	static private void printChildren(IStatus parent, PrintWriter output, StackTraceElement[] topStack, int level) {
-		IStatus[] children = parent.getChildren();
+		IStatus[] children = status.getChildren();
 		if (children == null || children.length == 0)
 			return;
-
 		for (int i = 0; i < children.length; i++) {
-			output.println("Caused by: " + children[i].getMessage()); //$NON-NLS-1$
+			output.println("Contains: " + children[i].getMessage()); //$NON-NLS-1$
+			output.flush(); // call to synchronize output
 			Throwable exception = children[i].getException();
-			if (exception != null) {
-				StackTraceElement[] stack = exception.getStackTrace();
-				if (stack == null || stack.length == 0)
-					continue;
-				// eliminate common trails
-				int stackMatch = stack.length;
-				int topPosition = (topStack == null) ? 0 : topStack.length;
-				for (int j = stack.length; j > 0 && topPosition > 0; j--, topPosition--) {
-					if (!stack[j - 1].equals(topStack[topPosition - 1])) {
-						stackMatch = j;
-						break;
-					}
-				}
-				for (int j = 0; j < stackMatch; j++)
-					output.println("\tat " + stack[j]); //$NON-NLS-1$
-				if (stackMatch != stack.length)
-					output.println("\t... " + Integer.toString(stack.length - stackMatch) + " more"); //$NON-NLS-1$ //$NON-NLS-2$
-			}
-			printChildren(children[i], output, topStack, level + 1);
+			if (exception != null)
+				exception.printStackTrace();
+			printChildren(children[i], output);
 		}
 	}
 
