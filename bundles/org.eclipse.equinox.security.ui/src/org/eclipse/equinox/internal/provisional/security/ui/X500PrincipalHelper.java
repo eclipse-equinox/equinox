6ff109bc463ca0fa38d6d2d1e9eded794379c064
Thomas Watson <twatson>
2008-06-03 01:03:03 +0000

    update copyrights

--- bundles/org.eclipse.equinox.security.ui/src/org/eclipse/equinox/internal/provisional/security/ui/X500PrincipalHelper.java
+++ bundles/org.eclipse.equinox.security.ui/src/org/eclipse/equinox/internal/provisional/security/ui/X500PrincipalHelper.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008,  Jay Rosenthal
+ * Copyright (c) 2008,  Jay Rosenthal and others.
  * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
