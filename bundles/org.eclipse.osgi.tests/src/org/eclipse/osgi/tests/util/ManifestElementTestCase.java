a7bbd61993d0b845b9622c9c3aa0d76d52295ab5
Thomas Watson <twatson>
2008-07-01 16:42:09 +0000

    Bug 238675 Bundle-NativeCode parsing error

--- bundles/org.eclipse.osgi.tests/src/org/eclipse/osgi/tests/util/ManifestElementTestCase.java
+++ bundles/org.eclipse.osgi.tests/src/org/eclipse/osgi/tests/util/ManifestElementTestCase.java
@@ -11,10 +11,9 @@
 
 package org.eclipse.osgi.tests.util;
 
+import junit.framework.TestCase;
 import org.eclipse.osgi.util.ManifestElement;
 import org.osgi.framework.BundleException;
-
-import junit.framework.TestCase;
 
 public class ManifestElementTestCase extends TestCase {
 
@@ -33,4 +32,25 @@ public class ManifestElementTestCase extends TestCase {
 		assertEquals("3.1", elements[0].getDirective("dir"), "val 2");
 	}
 
+	public void testBug238675_01() throws BundleException {
+		ManifestElement[] elements = ManifestElement.parseHeader("Bundle-NativeCode", "\"external:C:/tmp/x.dll\";\"external:C:/tmp/y.dll\"; osname =WindowsXP; osverison = 2.0; processor = x86"); //$NON-NLS-1$//$NON-NLS-2$
+		assertNotNull("1.0", elements);
+		assertEquals("1.1", 1, elements.length);
+		String[] components = elements[0].getValueComponents();
+		assertEquals("1.2", 2, components.length);
+
+		assertEquals("2.0", "external:C:/tmp/x.dll", components[0]);
+		assertEquals("2.1", "external:C:/tmp/y.dll", components[1]);
+	}
+
+	public void testBug238675_02() throws BundleException {
+		ManifestElement[] elements = ManifestElement.parseHeader("Bundle-NativeCode", "\"external:test1:test2\";\"test3:test4:\"; osname =WindowsXP; osverison = 2.0; processor = x86"); //$NON-NLS-1$ //$NON-NLS-2$
+		assertNotNull("1.0", elements);
+		assertEquals("1.1", 1, elements.length);
+		String[] components = elements[0].getValueComponents();
+		assertEquals("1.2", 2, components.length);
+
+		assertEquals("2.0", components[0], "external:test1:test2");
+		assertEquals("2.1", components[1], "test3:test4:");
+	}
 }
