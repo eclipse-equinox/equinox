7e2fa05c41b1f235eda6cb8728a48b6e7c8ab5aa
Thomas Watson <twatson>
2008-12-15 15:20:32 +0000

    Bug 258802 Invalid implementation of getSigner method in LegacyVerifierFactory.LegacyChain class

--- bundles/org.eclipse.osgi/security/src/org/eclipse/osgi/internal/signedcontent/LegacyVerifierFactory.java
+++ bundles/org.eclipse.osgi/security/src/org/eclipse/osgi/internal/signedcontent/LegacyVerifierFactory.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007 IBM Corporation and others. All rights reserved.
+ * Copyright (c) 2007, 2008 IBM Corporation and others. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
@@ -123,7 +123,7 @@ public class LegacyVerifierFactory implements CertificateVerifierFactory {
 
 		public Certificate getSigner() {
 			Certificate[] certs = getCertificates();
-			return certs.length > 0 ? certs[1] : null;
+			return certs.length > 0 ? certs[0] : null;
 		}
 
 		public Date getSigningTime() {
