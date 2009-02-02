ff3b506e1421d58648b92a6f171bf81bdd1261ee
Thomas Watson <twatson>
2009-02-02 20:11:47 +0000

    Bug 241663 [user admin] Incorrect Password Save/Load

--- bundles/org.eclipse.equinox.compendium.tests/src/org/eclipse/equinox/useradmin/tests/UserTest.java
+++ bundles/org.eclipse.equinox.compendium.tests/src/org/eclipse/equinox/useradmin/tests/UserTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008 IBM Corporation and others
+ * Copyright (c) 2008, 2009 IBM Corporation and others
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,6 +10,7 @@
  *******************************************************************************/
 package org.eclipse.equinox.useradmin.tests;
 
+import java.util.Dictionary;
 import junit.framework.TestCase;
 import org.eclipse.equinox.compendium.tests.Activator;
 import org.osgi.framework.InvalidSyntaxException;
@@ -40,18 +41,18 @@ public class UserTest extends TestCase {
 		assertNotNull(user);
 		assertEquals("testUserCreate1", user.getName()); //$NON-NLS-1$
 		assertTrue(user.getType() == Role.USER);
-		user.getProperties().put("test", "valu");
+		user.getProperties().put("test", "valu"); //$NON-NLS-1$ //$NON-NLS-2$
 	}
 
 	public void testGetUser() {
-		User user = userAdmin.getUser("test", "valu");
+		User user = userAdmin.getUser("test", "valu"); //$NON-NLS-1$ //$NON-NLS-2$
 		assertNotNull(user);
 		assertEquals("testUserCreate1", user.getName()); //$NON-NLS-1$
 		assertTrue(user.getType() == Role.USER);
 		try {
-			Role[] roles = userAdmin.getRoles("(test=valu)");
+			Role[] roles = userAdmin.getRoles("(test=valu)"); //$NON-NLS-1$
 			assertNotNull(roles);
-			assertEquals("number of roles", 1, roles.length);
+			assertEquals("number of roles", 1, roles.length); //$NON-NLS-1$
 		} catch (InvalidSyntaxException e) {
 			fail(e.getMessage());
 		}
@@ -62,13 +63,72 @@ public class UserTest extends TestCase {
 		assertNull(userAdmin.getRole("testUserCreate1")); //$NON-NLS-1$
 	}
 
-	public void testUserCreateAndRemove() throws Exception {
-		User user = (User) userAdmin.createRole("testUserCreate", Role.USER); //$NON-NLS-1$
+	public void testCreate02() throws Exception {
+		User user = (User) userAdmin.createRole("testUserCreate2", Role.USER); //$NON-NLS-1$
 		assertNotNull(user);
-		assertEquals("testUserCreate", user.getName()); //$NON-NLS-1$
+		assertEquals("testUserCreate2", user.getName()); //$NON-NLS-1$
 		assertTrue(user.getType() == Role.USER);
-		assertTrue(userAdmin.removeRole("testUserCreate")); //$NON-NLS-1$
-		assertNull(userAdmin.getRole("testUserCreate")); //$NON-NLS-1$
+		user.getProperties().put("test1", "valu"); //$NON-NLS-1$ //$NON-NLS-2$
+		user.getProperties().put("test2", "xxxyyyzzz"); //$NON-NLS-1$ //$NON-NLS-2$
+		user.getProperties().put("test3", new byte[] {1, 2, 3}); //$NON-NLS-1$
+
+		user.getCredentials().put("test1", "valu"); //$NON-NLS-1$ //$NON-NLS-2$
+		user.getCredentials().put("test2", "xxxyyyzzz"); //$NON-NLS-1$ //$NON-NLS-2$
+		user.getCredentials().put("test3", new byte[] {1, 2, 3}); //$NON-NLS-1$
+	}
+
+	public void testGetUser02() {
+		Role user = userAdmin.getRole("testUserCreate2"); //$NON-NLS-1$
+		assertNotNull(user);
+		assertEquals("testUserCreate2", user.getName()); //$NON-NLS-1$
+		assertTrue(user.getType() == Role.USER);
+
+		Object test1 = user.getProperties().get("test1"); //$NON-NLS-1$
+		assertEquals("test1", "valu", test1); //$NON-NLS-1$ //$NON-NLS-2$
+		Object test2 = user.getProperties().get("test2"); //$NON-NLS-1$
+		assertEquals("test2", "xxxyyyzzz", test2); //$NON-NLS-1$ //$NON-NLS-2$
+		Object test3 = user.getProperties().get("test3"); //$NON-NLS-1$
+		assertTrue("test3 not byte[]", test3 instanceof byte[]); //$NON-NLS-1$
+		byte[] bytes = (byte[]) test3;
+		assertEquals("wrong size", 3, bytes.length); //$NON-NLS-1$
+		assertEquals("1", 1, bytes[0]); //$NON-NLS-1$
+		assertEquals("2", 2, bytes[1]); //$NON-NLS-1$
+		assertEquals("3", 3, bytes[2]); //$NON-NLS-1$
+
+		Dictionary creds = ((User) user).getCredentials();
+		test1 = creds.get("test1"); //$NON-NLS-1$
+		assertEquals("test1", "valu", test1); //$NON-NLS-1$ //$NON-NLS-2$
+		test2 = creds.get("test2"); //$NON-NLS-1$
+		assertEquals("test2", "xxxyyyzzz", test2); //$NON-NLS-1$ //$NON-NLS-2$
+		test3 = creds.get("test3"); //$NON-NLS-1$
+		assertTrue("test3 not byte[]", test3 instanceof byte[]); //$NON-NLS-1$
+		bytes = (byte[]) test3;
+		assertEquals("wrong size", 3, bytes.length); //$NON-NLS-1$
+		assertEquals("1", 1, bytes[0]); //$NON-NLS-1$
+		assertEquals("2", 2, bytes[1]); //$NON-NLS-1$
+		assertEquals("3", 3, bytes[2]); //$NON-NLS-1$
+
+		try {
+			Role[] roles = userAdmin.getRoles("(test3=1)"); //$NON-NLS-1$
+			assertNotNull(roles);
+			assertEquals("number of roles", 1, roles.length); //$NON-NLS-1$
+		} catch (InvalidSyntaxException e) {
+			fail(e.getMessage());
+		}
+		userAdmin.removeRole(user.getName());
+	}
+
+	public void testRemovedUser02() {
+		assertNull(userAdmin.getRole("testUserCreate2")); //$NON-NLS-1$
+	}
+
+	public void testUserCreateAndRemove() throws Exception {
+		User user = (User) userAdmin.createRole("testUserCreateAndRemove", Role.USER); //$NON-NLS-1$
+		assertNotNull(user);
+		assertEquals("testUserCreateAndRemove", user.getName()); //$NON-NLS-1$
+		assertTrue(user.getType() == Role.USER);
+		assertTrue(userAdmin.removeRole("testUserCreateAndRemove")); //$NON-NLS-1$
+		assertNull(userAdmin.getRole("testUserCreateAndRemove")); //$NON-NLS-1$
 	}
 
 }
