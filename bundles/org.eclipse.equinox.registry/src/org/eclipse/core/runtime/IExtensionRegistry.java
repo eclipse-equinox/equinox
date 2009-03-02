c6f148a2f853068bb7a86ddc0e2a29595e5b50aa
Thomas Watson <twatson>
2009-03-02 15:37:53 +0000

    Bug 266601 Typo in javadoc of IExtensionRegistry#getConfigurationElementsFor

--- bundles/org.eclipse.equinox.registry/src/org/eclipse/core/runtime/IExtensionRegistry.java
+++ bundles/org.eclipse.equinox.registry/src/org/eclipse/core/runtime/IExtensionRegistry.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2003, 2008 IBM Corporation and others.
+ * Copyright (c) 2003, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -135,7 +135,7 @@ public interface IExtensionRegistry {
 	 * @param extensionPointName the simple identifier of the 
 	 *		extension point (e.g. <code>"builders"</code>)
 	 * @param extensionId the unique identifier of the extension 
-	 *		(e.g. <code>"com.example.acme.coolbuilder</code>)
+	 *		(e.g. <code>"com.example.acme.coolbuilder"</code>)
 	 * @return the configuration elements
 	 */
 	public IConfigurationElement[] getConfigurationElementsFor(String namespace, String extensionPointName, String extensionId);
