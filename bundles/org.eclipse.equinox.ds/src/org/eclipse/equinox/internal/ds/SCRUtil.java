3014876b3efffc82bb366176071a1eab0932a10c
sboshev <>
2009-03-05 10:14:11 +0000

    added new method checkMethodAccess

--- bundles/org.eclipse.equinox.ds/src/org/eclipse/equinox/internal/ds/SCRUtil.java
+++ bundles/org.eclipse.equinox.ds/src/org/eclipse/equinox/internal/ds/SCRUtil.java
@@ -12,9 +12,9 @@
 package org.eclipse.equinox.internal.ds;
 
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.util.Dictionary;
 import java.util.Enumeration;
-
 import org.eclipse.equinox.internal.util.pool.ObjectCreator;
 import org.eclipse.equinox.internal.util.pool.ObjectPool;
 
@@ -64,6 +64,40 @@ public final class SCRUtil implements ObjectCreator {
 			key = e.nextElement();
 			dst.put(key, src.get(key));
 		}
+	}
+
+	/**
+	 * Checks whether the method can be accessed according to the DS v1.1 specification rules (112.8.4 Locating Component Methods)
+	 * @param implClass the component implementation class
+	 * @param currentClass the class where the method is located. This class is in the component imlpementation class hierarchy
+	 * @param methodToCheck the method to be checked
+	 * @param isComponent11 specifies whether the component is according to schema 1.1 or 1.0. Its value is true in case it is a v1.1 component
+	 * @return true, if the method can be executed
+	 */
+	public static boolean checkMethodAccess(Class implClass, Class currentClass, Method methodToCheck, boolean isComponent11) {
+		int modifiers = methodToCheck.getModifiers();
+		boolean result = true;
+		if (isComponent11) {
+			if (currentClass == implClass) {
+				//the method is located in the component impl class
+				//allow all types of modifiers
+			} else {
+				//the method is located in a super class of the component impl class
+				if (Modifier.isPrivate(modifiers)) {
+					// private method - no access
+					result = false;
+				} else if (!Modifier.isPublic(modifiers) && !Modifier.isProtected(modifiers)) {
+					// not protected neither public neither private - this is private package case
+					if (currentClass.getPackage() != implClass.getPackage()) {
+						//do not accept the method if its class package differs the package of the component impl class 
+						result = false;
+					}
+				}
+			}
+		} else {
+			result = Modifier.isPublic(modifiers) || Modifier.isProtected(modifiers);
+		}
+		return result;
 	}
 
 	private static Method setAccessibleMethod = null;
