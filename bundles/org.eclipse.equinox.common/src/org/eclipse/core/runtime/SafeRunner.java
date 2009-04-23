2f7b5a50961ac729ae0f259484bc1bb523734edc
John Arthorne <johna>
2009-04-23 14:21:09 +0000

    Bug 273141 Catch AssertionError in SafeRunner

--- bundles/org.eclipse.equinox.common/src/org/eclipse/core/runtime/SafeRunner.java
+++ bundles/org.eclipse.equinox.common/src/org/eclipse/core/runtime/SafeRunner.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2006 IBM Corporation and others.
+ * Copyright (c) 2005, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -38,6 +38,8 @@ public final class SafeRunner {
 		} catch (Exception e) {
 			handleException(code, e);
 		} catch (LinkageError e) {
+			handleException(code, e);
+		} catch (AssertionError e) {
 			handleException(code, e);
 		}
 	}
