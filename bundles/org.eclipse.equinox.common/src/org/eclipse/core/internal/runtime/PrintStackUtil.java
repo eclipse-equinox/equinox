c35f2bed364bca2a5fd1a76cec3c4c2c0dcc5ae3
John Arthorne <johna>
2009-05-13 19:14:59 +0000

    Bug 276076 [common] Stack trace is always printed to the console

--- bundles/org.eclipse.equinox.common/src/org/eclipse/core/internal/runtime/PrintStackUtil.java
+++ bundles/org.eclipse.equinox.common/src/org/eclipse/core/internal/runtime/PrintStackUtil.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008 IBM Corporation and others.
+ * Copyright (c) 2008, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -24,7 +24,7 @@ public class PrintStackUtil {
 			output.println("Contains: " + children[i].getMessage()); //$NON-NLS-1$
 			Throwable exception = children[i].getException();
 			if (exception != null)
-				exception.printStackTrace();
+				exception.printStackTrace(output);
 			printChildren(children[i], output);
 		}
 	}
@@ -38,7 +38,7 @@ public class PrintStackUtil {
 			output.flush(); // call to synchronize output
 			Throwable exception = children[i].getException();
 			if (exception != null)
-				exception.printStackTrace();
+				exception.printStackTrace(output);
 			printChildren(children[i], output);
 		}
 	}
