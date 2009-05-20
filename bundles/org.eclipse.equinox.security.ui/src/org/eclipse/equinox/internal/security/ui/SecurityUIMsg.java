843e0ff8f151ec89bee3c9138af2b3236a352590
Thomas Watson <twatson>
2009-05-20 22:02:56 +0000

    update copyrights

--- bundles/org.eclipse.equinox.security.ui/src/org/eclipse/equinox/internal/security/ui/SecurityUIMsg.java
+++ bundles/org.eclipse.equinox.security.ui/src/org/eclipse/equinox/internal/security/ui/SecurityUIMsg.java
@@ -1,9 +1,9 @@
 /*******************************************************************************
- * Copyright (c) 2007 IBM Corporation and others_
- * All rights reserved_ This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1_0
+ * Copyright (c) 2007, 2008 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www_eclipse_org/legal/epl-v10_html
+ * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
  *     IBM Corporation - initial API and implementation
