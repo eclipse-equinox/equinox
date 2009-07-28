e71978093cba005ae314447abdbdea8c70a74ed7
Thomas Watson <twatson>
2009-07-28 20:11:22 +0000

    Bug 260217 Installing a bundle by reference without having read permission to the file results in a confusing exception

--- bundles/org.eclipse.osgi/defaultAdaptor/src/org/eclipse/osgi/framework/internal/protocol/reference/ReferenceURLConnection.java
+++ bundles/org.eclipse.osgi/defaultAdaptor/src/org/eclipse/osgi/framework/internal/protocol/reference/ReferenceURLConnection.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2003, 2006 IBM Corporation and others.
+ * Copyright (c) 2003, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -44,6 +44,8 @@ public class ReferenceURLConnection extends URLConnection {
 			ref = file.toURL();
 			if (!file.exists())
 				throw new FileNotFoundException(file.toString());
+			if (!file.canRead())
+				throw new FileNotFoundException(file.toString() + " (access denied)"); //$NON-NLS-1$
 			reference = ref;
 		}
 	}
