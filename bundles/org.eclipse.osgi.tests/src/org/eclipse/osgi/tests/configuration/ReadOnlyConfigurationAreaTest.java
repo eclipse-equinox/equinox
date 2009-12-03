fe6b645a63154ad50ddb192da8610fd7628b06ce
Thomas Watson <twatson>
2009-12-03 20:06:10 +0000

    Bug 296846 -  [tests] assert bundle resolution happens

--- bundles/org.eclipse.osgi.tests/src/org/eclipse/osgi/tests/configuration/ReadOnlyConfigurationAreaTest.java
+++ bundles/org.eclipse.osgi.tests/src/org/eclipse/osgi/tests/configuration/ReadOnlyConfigurationAreaTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005 IBM Corporation and others.
+ * Copyright (c) 2005, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -43,9 +43,10 @@ public class ReadOnlyConfigurationAreaTest extends OSGiTest {
 		try {
 			Bundle installed = BundleTestingHelper.installBundle("1.0", getContext(), OSGiTestsActivator.TEST_FILES_ROOT + "configuration/bundle01");
 			// not read-only yet, should work fine
-			BundleTestingHelper.resolveBundles(getContext(), new Bundle[] {installed});
+			if (!BundleTestingHelper.resolveBundles(getContext(), new Bundle[] {installed}))
+				fail("1.1");
 		} catch (BundleException be) {
-			fail("1.0", be);
+			fail("1.2", be);
 		}
 	}
 
