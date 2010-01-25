ba4699d9f9c00d7174574769cfe7a15dbbe160eb
DJ Houghton <dj>
2010-01-25 17:04:56 +0000

    Bug 300197 - PlatformURLPluginConnection#parse fails to load bundle with underscore in Bundle-SymbolicName

--- bundles/org.eclipse.equinox.common/src/org/eclipse/core/internal/boot/PlatformURLConnection.java
+++ bundles/org.eclipse.equinox.common/src/org/eclipse/core/internal/boot/PlatformURLConnection.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2009 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -336,7 +336,6 @@ public abstract class PlatformURLConnection extends URLConnection {
 		Version version = null;
 		int underScore = spec.indexOf('_');
 		while (underScore >= 0) {
-			bsn = spec.substring(0, underScore);
 			try {
 				version = Version.parseVersion(spec.substring(underScore + 1));
 			} catch (IllegalArgumentException iae) {
@@ -344,6 +343,7 @@ public abstract class PlatformURLConnection extends URLConnection {
 				underScore = spec.indexOf('_', underScore + 1);
 				continue;
 			}
+			bsn = spec.substring(0, underScore);
 			break;
 		}
 		return new Object[] {bsn, version};
