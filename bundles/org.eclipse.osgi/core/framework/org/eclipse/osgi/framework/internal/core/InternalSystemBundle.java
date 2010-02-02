c54cf398d4bcb07fef4882a8d142960c1dad0db6
Thomas Watson <twatson>
2010-02-02 21:47:38 +0000

    Include cause in exception.

--- bundles/org.eclipse.osgi/core/framework/org/eclipse/osgi/framework/internal/core/InternalSystemBundle.java
+++ bundles/org.eclipse.osgi/core/framework/org/eclipse/osgi/framework/internal/core/InternalSystemBundle.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2003, 2009 IBM Corporation and others.
+ * Copyright (c) 2003, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -212,7 +212,7 @@ public class InternalSystemBundle extends BundleHost implements org.osgi.framewo
 				Debug.printStackTrace(be.getNestedException() == null ? be : be.getNestedException());
 			}
 			framework.publishFrameworkEvent(FrameworkEvent.ERROR, framework.systemBundle, be);
-			throw new RuntimeException(be.getMessage());
+			throw new RuntimeException(be.getMessage(), be);
 		}
 
 	}
