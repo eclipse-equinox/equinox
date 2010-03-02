2823d9e5edf97ea81143d99d4524dcc769416f74
Thomas Watson <twatson>
2010-03-02 23:15:20 +0000

    Bug 304361 -  [app] enhance the eclipse application container to allow for async application results

--- bundles/org.eclipse.osgi.tests/src/org/eclipse/osgi/tests/appadmin/ExitValueApp.java
+++ bundles/org.eclipse.osgi.tests/src/org/eclipse/osgi/tests/appadmin/ExitValueApp.java
@@ -13,27 +13,48 @@ package org.eclipse.osgi.tests.appadmin;
 import org.eclipse.equinox.app.IApplication;
 import org.eclipse.equinox.app.IApplicationContext;
 
-public class ExitValueApp implements IApplication {
-	public static final String returnNullResult = "return null"; //$NON-NLS-1$
+public class ExitValueApp implements IApplication, Runnable {
+	public static final String returnNullResult = "return.null"; //$NON-NLS-1$
+	public static final String returnAsyncResult = "return.async";
+	public static final String setAsyncEarly = "set.async.early";
+	public static final String setAsyncWrongApp = "set.async.wrongApp";
 	public static final String exitValue = "Exit Value"; //$NON-NLS-1$
+
 	private boolean active = true;
 	private boolean stopped = false;
+	private boolean useAsync = false;
+	private boolean returnNull = false;
+	private boolean setWrongApp = false;
+	private IApplicationContext appContext;
+	final Object guardObj = new Object();
 
 	public synchronized Object start(IApplicationContext context) {
-		boolean returnNull = false;
+		appContext = context;
 		context.applicationRunning();
-		if (active) {
-			String[] args = (String[]) context.getArguments().get(IApplicationContext.APPLICATION_ARGS);
-			if (args != null && args.length > 0)
-				returnNull = returnNullResult.equals(args[0]);
+		Boolean nullValue = (Boolean) context.getArguments().get(returnNullResult);
+		returnNull = nullValue == null ? false : nullValue.booleanValue();
+		Boolean asyncValue = (Boolean) context.getArguments().get(returnAsyncResult);
+		useAsync = asyncValue == null ? false : asyncValue.booleanValue();
+
+		Boolean asyncEarlyValue = (Boolean) context.getArguments().get(setAsyncEarly);
+		boolean setEarly = asyncEarlyValue == null ? false : asyncEarlyValue.booleanValue();
+		Boolean wrongAppValue = (Boolean) context.getArguments().get(setAsyncWrongApp);
+		setWrongApp = wrongAppValue == null ? false : wrongAppValue.booleanValue();
+		if (setEarly) {
 			try {
-				wait(5000); // only run for 5 seconds at most
-			} catch (InterruptedException e) {
-				// do nothing
+				context.setResult("failed", this);
+				// failed
+			} catch (IllegalStateException e) {
+				// passed
+				return exitValue;
 			}
 		}
-		stopped = true;
-		notifyAll();
+		if (useAsync) {
+			System.out.println("async result");
+			new Thread(this, "ExitValueApp Test").start();
+			return IApplicationContext.EXIT_ASYNC_RESULT;
+		}
+		run();
 		return returnNull ? null : exitValue;
 	}
 
@@ -46,6 +67,42 @@ public class ExitValueApp implements IApplication {
 			} catch (InterruptedException e) {
 				// do nothing
 			}
+	}
+
+	public synchronized void run() {
+		if (active) {
+			try {
+				wait(5000); // only run for 5 seconds at most
+			} catch (InterruptedException e) {
+				// do nothing
+			}
+		}
+		stopped = true;
+		if (useAsync) {
+			IApplication app = this;
+			Object result = returnNull ? null : exitValue;
+			if (setWrongApp) {
+				result = "failed";
+				app = new IApplication() {
+
+					public void stop() {
+						// nothing
+					}
+
+					public Object start(IApplicationContext context) throws Exception {
+						return null;
+					}
+				};
+			}
+			try {
+				appContext.setResult(result, app);
+				// failed
+			} catch (IllegalArgumentException e) {
+				// passed
+				appContext.setResult(returnNull ? null : exitValue, this);
+			}
+		}
+		notifyAll();
 	}
 
 }
