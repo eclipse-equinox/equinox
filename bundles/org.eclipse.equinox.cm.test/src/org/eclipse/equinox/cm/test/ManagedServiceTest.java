62e7bb71c0cb4ff8b9737389e320bd1871d9b15b
Simon Kaegi <skaegi>
2010-04-22 03:45:16 +0000

    Bug 288253 - ConfigurationAdmin after Configuration.setLocation() change: ManagedService instance not called when PID is updated

--- bundles/org.eclipse.equinox.cm.test/src/org/eclipse/equinox/cm/test/ManagedServiceTest.java
+++ bundles/org.eclipse.equinox.cm.test/src/org/eclipse/equinox/cm/test/ManagedServiceTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007 IBM Corporation and others
+ * Copyright (c) 2007, 2010 IBM Corporation and others
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -125,6 +125,18 @@ public class ManagedServiceTest extends TestCase {
 				fail("should have updated");
 			assertEquals(2, updateCount);
 		}
+
+		String location = config.getBundleLocation();
+		config.setBundleLocation("bogus");
+		synchronized (lock) {
+			config.update();
+			locked = true;
+			lock.wait(100);
+			assertTrue(locked);
+			assertEquals(2, updateCount);
+			locked = false;
+		}
+		config.setBundleLocation(location);
 
 		dict.remove(Constants.SERVICE_PID);
 		synchronized (lock) {
