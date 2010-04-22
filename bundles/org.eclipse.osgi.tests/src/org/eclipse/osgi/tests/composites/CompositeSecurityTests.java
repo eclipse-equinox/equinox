9cce00cb43a8389202dd4394f94bfd18d68ff8ce
Thomas Watson <twatson>
2010-04-22 14:58:20 +0000

    Bug 310124 -  AccessControlException when running eclipse tests

--- bundles/org.eclipse.osgi.tests/src/org/eclipse/osgi/tests/composites/CompositeSecurityTests.java
+++ bundles/org.eclipse.osgi.tests/src/org/eclipse/osgi/tests/composites/CompositeSecurityTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008 IBM Corporation and others.
+ * Copyright (c) 2008, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -95,6 +95,8 @@ public class CompositeSecurityTests extends AbstractCompositeTests {
 		startCompositeBundle(composite, false);
 
 		stopCompositeBundle(composite);
+
+		uninstallCompositeBundle(composite);
 	}
 
 	public void testLinkSecurity02() {
