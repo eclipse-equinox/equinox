2ee362c4d4826cd103503c26d2750566141732e4
Andrew Niefer <aniefer>
2010-06-17 15:16:01 +0000

    bug 317200 - crash in formatVmCommandMsg

--- bundles/org.eclipse.equinox.executable/library/eclipse.c
+++ bundles/org.eclipse.equinox.executable/library/eclipse.c
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2009 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at 
@@ -1003,7 +1003,7 @@ static _TCHAR*  formatVmCommandMsg( _TCHAR* args[], _TCHAR* vmArgs[], _TCHAR* pr
 	while(list != NULL) {
 		for (index = 0; list[index] != NULL; index++)
 		{
-			if (list[index][0] == _T_ECLIPSE('-') && *(ch-1) == _T_ECLIPSE(' '))
+			if (ch != message && list[index][0] == _T_ECLIPSE('-') && *(ch-1) == _T_ECLIPSE(' '))
 				*(ch-1) = _T_ECLIPSE('\n');
 			_tcscpy( ch, list[index] );
 			ch += _tcslen( list[index] );
