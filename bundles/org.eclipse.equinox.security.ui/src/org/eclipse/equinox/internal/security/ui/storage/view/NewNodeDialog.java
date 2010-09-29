5a99cfc6dc519f128a4f9d798ab635a0eed58543
Oleg Besedin <obesedin>
2010-09-29 13:46:03 +0000

    Bug 326504 - [patch] useless composite in NewNodeDialog.createDialogArea

--- bundles/org.eclipse.equinox.security.ui/src/org/eclipse/equinox/internal/security/ui/storage/view/NewNodeDialog.java
+++ bundles/org.eclipse.equinox.security.ui/src/org/eclipse/equinox/internal/security/ui/storage/view/NewNodeDialog.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008 IBM Corporation and others.
+ * Copyright (c) 2008, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -47,9 +47,7 @@ public class NewNodeDialog extends TitleAreaDialog {
 	}
 
 	protected Control createDialogArea(Composite parent) {
-		Composite compositeTop = (Composite) super.createDialogArea(parent);
-		Composite composite = new Composite(compositeTop, SWT.NONE);
-
+		Composite composite = (Composite) super.createDialogArea(parent);
 		setMessage(SecUIMessages.newNodeMsg);
 
 		new Label(composite, SWT.LEFT).setText(SecUIMessages.newNodeLabel);
