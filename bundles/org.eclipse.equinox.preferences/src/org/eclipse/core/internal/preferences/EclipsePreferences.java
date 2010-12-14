52290f83b95742dd35750bf35317afeebe387d11
DJ Houghton <dj>
2010-12-14 16:10:48 +0000

    Bug 332503 - [prefs] EclipsePreferences.toString() changes the state of object under debug

--- bundles/org.eclipse.equinox.preferences/src/org/eclipse/core/internal/preferences/EclipsePreferences.java
+++ bundles/org.eclipse.equinox.preferences/src/org/eclipse/core/internal/preferences/EclipsePreferences.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2009 IBM Corporation and others.
+ * Copyright (c) 2004, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -79,6 +79,7 @@ public class EclipsePreferences implements IEclipsePreferences, IScope {
 		super();
 		this.parent = parent;
 		this.name = name;
+		this.cachedPath = null; // make sure the cached path is cleared after setting the parent
 	}
 
 	/*
