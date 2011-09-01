eee57ae43701a0495f3614a85582a517e55fad88
Thomas Watson <tjwatson@us.ibm.com>
2011-09-01 16:02:37 -0500

    Bug 356529 - [region] VISIBLE_BUNDLE_NAMESPACE javadoc states bundle-symbolic-name attribute is used

--- bundles/org.eclipse.equinox.region/src/org/eclipse/equinox/internal/region/StandardRegionFilter.java
+++ bundles/org.eclipse.equinox.region/src/org/eclipse/equinox/internal/region/StandardRegionFilter.java
@@ -42,8 +42,12 @@ final class StandardRegionFilter implements RegionFilter {
 	public boolean isAllowed(Bundle bundle) {
 		HashMap<String, Object> attrs = new HashMap<String, Object>(3);
 		String bsn = bundle.getSymbolicName();
-		if (bsn != null)
+		if (bsn != null) {
+			// TODO the javadoc says to use the bundle-symbolic-name attribute; leaving this for behavior compatibility
 			attrs.put(VISIBLE_BUNDLE_NAMESPACE, bsn);
+			// This is the correct attribute to use according to the javadoc
+			attrs.put(Constants.BUNDLE_SYMBOLICNAME_ATTRIBUTE, bsn);
+		}
 		attrs.put(org.osgi.framework.Constants.BUNDLE_VERSION_ATTRIBUTE, bundle.getVersion());
 		attrs.put(BUNDLE_ID_ATTR, bundle.getBundleId());
 		return isBundleAllowed(attrs);
