34170596b63a67e8aa4227470b241d381564ff3d
Oleg Besedin <obesedin@ca.ibm.com>
2012-03-19 11:11:43 -0400

    Bug 374466 - NPE from PasswordProviderSelector.getInstance when used without start levels

--- bundles/org.eclipse.equinox.security/src/org/eclipse/equinox/internal/security/auth/AuthPlugin.java
+++ bundles/org.eclipse.equinox.security/src/org/eclipse/equinox/internal/security/auth/AuthPlugin.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2008 IBM Corporation and others.
+ * Copyright (c) 2005, 2012 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -70,7 +70,7 @@ public class AuthPlugin implements BundleActivator {
 
 	public void stop(BundleContext context) throws Exception {
 
-		PasswordProviderSelector.getInstance().stop();
+		PasswordProviderSelector.stop();
 		SecurePreferencesMapper.stop();
 		SecurePlatformInternal.getInstance().stop();
 
