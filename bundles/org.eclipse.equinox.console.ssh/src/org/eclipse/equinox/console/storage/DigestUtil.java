c9303791491aa30b09518e56fb64a7eb948a8f5f
Lazar Kirchev <l.kirchev@sap.com>
2012-04-30 16:56:58 +0300

    Update copiright headers of recently changed files

--- bundles/org.eclipse.equinox.console.ssh/src/org/eclipse/equinox/console/storage/DigestUtil.java
+++ bundles/org.eclipse.equinox.console.ssh/src/org/eclipse/equinox/console/storage/DigestUtil.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2011 SAP AG
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Lazar Kirchev, SAP AG - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.equinox.console.storage;
 
 import java.security.MessageDigest;
