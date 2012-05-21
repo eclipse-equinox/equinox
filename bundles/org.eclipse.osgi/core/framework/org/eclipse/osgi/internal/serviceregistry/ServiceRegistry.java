8ad704b0c8149a6ab23d96f414045543e171fa06
Thomas Watson <tjwatson@us.ibm.com>
2012-05-21 15:49:59 -0500

    Bug 379865 - Getting OSGi service references with simple filter is slow

--- bundles/org.eclipse.osgi/core/framework/org/eclipse/osgi/internal/serviceregistry/ServiceRegistry.java
+++ bundles/org.eclipse.osgi/core/framework/org/eclipse/osgi/internal/serviceregistry/ServiceRegistry.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2010 IBM Corporation and others.
+ * Copyright (c) 2004, 2012 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -963,7 +963,7 @@ public class ServiceRegistry {
 				return empty;
 			}
 
-			result = new ArrayList<ServiceRegistrationImpl<?>>(result); /* make a new list since we don't want to change the real list */
+			result = new LinkedList<ServiceRegistrationImpl<?>>(result); /* make a new list since we don't want to change the real list */
 		}
 
 		if (filter == null) {
