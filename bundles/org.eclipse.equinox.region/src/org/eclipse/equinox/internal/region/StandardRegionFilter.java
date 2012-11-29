3dce273198f8916e7a321b9529827f789b501d42
John Ross <jwross@us.ibm.com>
2012-11-29 08:24:15 -0600

    [Bug 395334] Update copyrights.

--- bundles/org.eclipse.equinox.region/src/org/eclipse/equinox/internal/region/StandardRegionFilter.java
+++ bundles/org.eclipse.equinox.region/src/org/eclipse/equinox/internal/region/StandardRegionFilter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2012 VMware Inc.
+ * Copyright (c) 2011, 2012 VMware Inc and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *   VMware Inc. - initial contribution
+ *   IBM - bug fixes and enhancements
  *******************************************************************************/
 
 package org.eclipse.equinox.internal.region;
