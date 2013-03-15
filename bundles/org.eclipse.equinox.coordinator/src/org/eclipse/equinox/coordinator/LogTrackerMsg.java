f9dd8a4adc0c1d014de7b3d47e46b1597a137ecb
John Ross <jwross@us.ibm.com>
2013-03-15 10:24:14 -0500

    Bug 403484 - [coordinator] NLS missing message: Error in: org.eclipse.equinox.internal.event.LogMessages
    Corrected the properties file name.

--- bundles/org.eclipse.equinox.coordinator/src/org/eclipse/equinox/coordinator/LogTrackerMsg.java
+++ bundles/org.eclipse.equinox.coordinator/src/org/eclipse/equinox/coordinator/LogTrackerMsg.java
@@ -13,7 +13,7 @@ package org.eclipse.equinox.coordinator;
 import org.eclipse.osgi.util.NLS;
 
 public class LogTrackerMsg extends NLS {
-	private static final String BUNDLE_NAME = "org.eclipse.equinox.internal.event.LogMessages"; //$NON-NLS-1$
+	private static final String BUNDLE_NAME = "org.eclipse.equinox.coordinator.LogMessages"; //$NON-NLS-1$
 
 	public static String Unknown_Log_level;
 	public static String Info;
