3c19054e26939de6dc8d70d87904d667536a70e6
Thomas Watson <tjwatson@us.ibm.com>
2013-05-22 08:13:54 -0500

    Bug 408629 - [unity] Preserve firing lazy start trigger when bundle is marked for eager start.
    - Added a testcase

--- bundles/org.eclipse.osgi.tests/src/org/eclipse/osgi/tests/bundles/ClassLoadingBundleTests.java
+++ bundles/org.eclipse.osgi.tests/src/org/eclipse/osgi/tests/bundles/ClassLoadingBundleTests.java
@@ -300,6 +300,77 @@ public class ClassLoadingBundleTests extends AbstractBundleTests {
 		}
 	}
 
+	public void testBug405918() throws BundleException {
+		Bundle chainTest = installer.installBundle("chain.test"); //$NON-NLS-1$
+		Bundle chainTestA = installer.installBundle("chain.test.a"); //$NON-NLS-1$
+		Bundle chainTestB = installer.installBundle("chain.test.b"); //$NON-NLS-1$
+		Bundle chainTestC = installer.installBundle("chain.test.c"); //$NON-NLS-1$
+		Bundle chainTestD = installer.installBundle("chain.test.d"); //$NON-NLS-1$
+		syncListenerResults.getResults(0);
+
+		StartLevel sl = installer.getStartLevel();
+		int currentSL = sl.getStartLevel();
+		int testSL = currentSL + 1;
+		sl.setBundleStartLevel(chainTest, testSL);
+		sl.setBundleStartLevel(chainTestA, testSL);
+		sl.setBundleStartLevel(chainTestB, testSL);
+		sl.setBundleStartLevel(chainTestC, testSL);
+		sl.setBundleStartLevel(chainTestD, testSL);
+		installer.resolveBundles(new Bundle[] {chainTest, chainTestA, chainTestB, chainTestC, chainTestD});
+
+		// eager start chainTestD
+		chainTestD.start();
+
+		Object[] expectedEvents = new Object[5];
+		expectedEvents[0] = new BundleEvent(BundleEvent.RESOLVED, chainTestD);
+		expectedEvents[1] = new BundleEvent(BundleEvent.RESOLVED, chainTestC);
+		expectedEvents[2] = new BundleEvent(BundleEvent.RESOLVED, chainTestB);
+		expectedEvents[3] = new BundleEvent(BundleEvent.RESOLVED, chainTestA);
+		expectedEvents[4] = new BundleEvent(BundleEvent.RESOLVED, chainTest);
+
+		Object[] actualEvents = syncListenerResults.getResults(5);
+		compareResults(expectedEvents, actualEvents);
+		try {
+			System.setProperty("test.bug300692", "true");
+			System.setProperty("test.bug300692.listener", "true");
+			chainTest.start();
+			sl.setStartLevel(testSL);
+			Object[] expectedFrameworkEvents = new Object[1];
+			expectedFrameworkEvents[0] = new FrameworkEvent(FrameworkEvent.STARTLEVEL_CHANGED, OSGiTestsActivator.getContext().getBundle(0), null);
+			Object[] actualFrameworkEvents = frameworkListenerResults.getResults(1);
+			compareResults(expectedFrameworkEvents, actualFrameworkEvents);
+
+			expectedEvents = new Object[14];
+			int i = 0;
+
+			expectedEvents[i++] = new BundleEvent(BundleEvent.LAZY_ACTIVATION, chainTestA);
+			expectedEvents[i++] = new BundleEvent(BundleEvent.LAZY_ACTIVATION, chainTestB);
+			expectedEvents[i++] = new BundleEvent(BundleEvent.LAZY_ACTIVATION, chainTestC);
+			expectedEvents[i++] = new BundleEvent(BundleEvent.STARTING, chainTest);
+			expectedEvents[i++] = new BundleEvent(BundleEvent.STARTING, chainTestB);
+			expectedEvents[i++] = new BundleEvent(BundleEvent.STARTED, chainTestB);
+			expectedEvents[i++] = new BundleEvent(BundleEvent.STARTING, chainTestC);
+			expectedEvents[i++] = new BundleEvent(BundleEvent.STARTED, chainTestC);
+			expectedEvents[i++] = new BundleEvent(BundleEvent.STARTING, chainTestA);
+			expectedEvents[i++] = new BundleEvent(BundleEvent.STARTED, chainTestA);
+			expectedEvents[i++] = new BundleEvent(BundleEvent.LAZY_ACTIVATION, chainTestD);
+			expectedEvents[i++] = new BundleEvent(BundleEvent.STARTING, chainTestD);
+			expectedEvents[i++] = new BundleEvent(BundleEvent.STARTED, chainTestD);
+			expectedEvents[i++] = new BundleEvent(BundleEvent.STARTED, chainTest);
+
+			actualFrameworkEvents = syncListenerResults.getResults(14);
+			compareResults(expectedEvents, actualFrameworkEvents);
+		} finally {
+			System.getProperties().remove("test.bug300692");
+			System.getProperties().remove("test.bug300692.listener");
+			sl.setStartLevel(currentSL);
+			Object[] expectedFrameworkEvents = new Object[1];
+			expectedFrameworkEvents[0] = new FrameworkEvent(FrameworkEvent.STARTLEVEL_CHANGED, OSGiTestsActivator.getContext().getBundle(0), null);
+			Object[] actualFrameworkEvents = frameworkListenerResults.getResults(1);
+			compareResults(expectedFrameworkEvents, actualFrameworkEvents);
+		}
+	}
+
 	public void testClassCircularityError() throws Exception {
 		Bundle circularityTest = installer.installBundle("circularity.test"); //$NON-NLS-1$
 		Bundle circularityTestA = installer.installBundle("circularity.test.a"); //$NON-NLS-1$
