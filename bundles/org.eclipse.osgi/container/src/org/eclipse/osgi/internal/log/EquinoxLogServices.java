4674a4912bc340c6b56a16eebbff87f4173832ff
Thomas Watson <tjwatson@us.ibm.com>
2013-06-27 14:22:37 -0500

    fix copyrights

--- bundles/org.eclipse.osgi/container/src/org/eclipse/osgi/internal/log/EquinoxLogServices.java
+++ bundles/org.eclipse.osgi/container/src/org/eclipse/osgi/internal/log/EquinoxLogServices.java
@@ -1,9 +1,5 @@
 /*******************************************************************************
-<<<<<<< HEAD
- * Copyright (c) 2006, 2012 IBM Corporation and others.
-=======
  * Copyright (c) 2006, 2013 IBM Corporation and others.
->>>>>>> master
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
