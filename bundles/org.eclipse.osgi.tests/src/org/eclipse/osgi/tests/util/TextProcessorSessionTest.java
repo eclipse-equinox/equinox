1070b693b75460f311be85953d0c7e9e0b35011a
Thomas Watson <tjwatson@us.ibm.com>
2013-07-01 14:08:04 -0500

    Change TextProcessorSessionTest to use its own configuration

--- bundles/org.eclipse.osgi.tests/src/org/eclipse/osgi/tests/util/TextProcessorSessionTest.java
+++ bundles/org.eclipse.osgi.tests/src/org/eclipse/osgi/tests/util/TextProcessorSessionTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 IBM Corporation and others.
+ * Copyright (c) 2011, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,13 +10,13 @@
  *******************************************************************************/
 package org.eclipse.osgi.tests.util;
 
-import org.eclipse.core.tests.session.SessionTestSuite;
-import org.eclipse.core.tests.session.Setup;
+import org.eclipse.core.tests.session.*;
 import org.eclipse.core.tests.session.SetupManager.SetupException;
+import org.eclipse.osgi.tests.OSGiTest;
 
-public class TextProcessorSessionTest extends SessionTestSuite {
+public class TextProcessorSessionTest extends ConfigurationSessionTestSuite {
 	private String lang = null;
-	
+
 	/**
 	 * Create a session test for the given class.  
 	 * @param pluginId tests plugin id
@@ -26,6 +26,10 @@ public class TextProcessorSessionTest extends SessionTestSuite {
 	public TextProcessorSessionTest(String pluginId, Class clazz, String language) {
 		super(pluginId, clazz);
 		lang = language;
+		String[] ids = ConfigurationSessionTestSuite.MINIMAL_BUNDLE_SET;
+		for (int i = 0; i < ids.length; i++)
+			addBundle(ids[i]);
+		addBundle(OSGiTest.PI_OSGI_TESTS);
 	}
 
 	/* (non-Javadoc)
@@ -34,7 +38,7 @@ public class TextProcessorSessionTest extends SessionTestSuite {
 	protected Setup newSetup() throws SetupException {
 		Setup base = super.newSetup();
 		// the base implementation will have set this to the host configuration
-		base.setEclipseArgument("nl", lang);	// Setup.NL is private
+		base.setEclipseArgument("nl", lang); // Setup.NL is private
 		return base;
 	}
 
