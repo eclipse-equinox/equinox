3636378bb84b70f6493de73531a28144c50f3ec0
John Ross <jwross@us.ibm.com>
2013-08-21 10:20:15 -0500

    [Bug 415461] The serializable class does not declare a static final serialVersionUID field of type long.
    Did not wish to change line 223 as the current way of constructing the ArrayList seemed crucial to the test.
    Could not use @SuppressAnnotations("serial") because the compiler compliance level is set to 1.4. We can change this
    but it would lead to the need for fixing a number of new warnings.
    
    Update copyright.

--- bundles/org.eclipse.equinox.cm.test/src/org/eclipse/equinox/cm/test/ConfigurationDictionaryTest.java
+++ bundles/org.eclipse.equinox.cm.test/src/org/eclipse/equinox/cm/test/ConfigurationDictionaryTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007 IBM Corporation and others
+ * Copyright (c) 2007, 2013 IBM Corporation and others
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -220,7 +220,9 @@ public class ConfigurationDictionaryTest extends TestCase {
 		config.update();
 		Dictionary dict = config.getProperties();
 		try {
-			Collection c = new ArrayList() {};
+			Collection c = new ArrayList() {
+				private static final long serialVersionUID = 1L;
+			};
 			dict.put("x", c);
 			config.update(dict);
 		} catch (IOException e) {
