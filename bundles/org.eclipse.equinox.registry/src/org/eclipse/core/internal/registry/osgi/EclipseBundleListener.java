d6315e1013aa93fd20fbc43e27a43d5d8f501811
Thomas Watson <tjwatson@us.ibm.com>
2014-03-24 10:48:15 -0500

    Bug 430821 - [registry] remove check that ignores system.bundle
    Change-Id: Ic391a28f177eb7c4f4658da3c7294fbfaaf344a1
    Signed-off-by: Thomas Watson <tjwatson@us.ibm.com>

--- bundles/org.eclipse.equinox.registry/src/org/eclipse/core/internal/registry/osgi/EclipseBundleListener.java
+++ bundles/org.eclipse.equinox.registry/src/org/eclipse/core/internal/registry/osgi/EclipseBundleListener.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2003, 2011 IBM Corporation and others.
+ * Copyright (c) 2003, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -108,9 +108,6 @@ public class EclipseBundleListener implements SynchronousBundleListener {
 	}
 
 	static public URL getExtensionURL(Bundle bundle, boolean report) {
-		// bail out if system bundle
-		if (bundle.getBundleId() == 0)
-			return null;
 		// bail out if the bundle does not have a symbolic name
 		if (bundle.getSymbolicName() == null)
 			return null;
