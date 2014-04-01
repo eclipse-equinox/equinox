011467683f36c2161bee717d1c05bd07fc0d51fe
Thomas Watson <tjwatson@us.ibm.com>
2014-04-01 16:19:59 -0500

    Bug 431727 - Overriding workspace location using -data does not work in RCP app

--- bundles/org.eclipse.osgi/container/src/org/eclipse/osgi/internal/framework/EquinoxConfiguration.java
+++ bundles/org.eclipse.osgi/container/src/org/eclipse/osgi/internal/framework/EquinoxConfiguration.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2003, 2013 IBM Corporation and others.
+ * Copyright (c) 2003, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -197,22 +197,7 @@ public class EquinoxConfiguration implements EnvironmentInfo {
 		Object useSystemPropsValue = initialConfig.get(PROP_USE_SYSTEM_PROPERTIES);
 		boolean useSystemProps = useSystemPropsValue == null ? false : Boolean.parseBoolean(useSystemPropsValue.toString());
 		this.configuration = useSystemProps ? System.getProperties() : new Properties();
-		if (useSystemProps) {
-			// system properties override initial configuration
-			for (Map.Entry<String, Object> configEntry : initialConfig.entrySet()) {
-				if (!this.configuration.containsKey(configEntry.getKey())) {
-					this.configuration.put(configEntry.getKey(), configEntry.getValue());
-				} else {
-					// override the initial config value
-					Object systemValue = this.configuration.get(configEntry.getKey());
-					if (systemValue != null) {
-						configEntry.setValue(systemValue);
-					}
-				}
-			}
-		} else {
-			this.configuration.putAll(initialConfig);
-		}
+		this.configuration.putAll(initialConfig);
 
 		initializeProperties(this.configuration, aliasMapper);
 		for (String initialKey : populateInitConfig) {
