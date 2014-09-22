e61bffabda5c428614b3b9cdba45620ed66cc6da
Thomas Watson <tjwatson@us.ibm.com>
2014-09-22 10:03:48 -0500

    Bug 444534 - Locale parsing of -nl parameter allows invalid values  - Add the new tests to the test suite.

--- bundles/org.eclipse.osgi.tests/src/org/eclipse/osgi/tests/eclipseadaptor/AllTests.java
+++ bundles/org.eclipse.osgi.tests/src/org/eclipse/osgi/tests/eclipseadaptor/AllTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005 IBM Corporation and others.
+ * Copyright (c) 2005, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,14 +10,14 @@
  *******************************************************************************/
 package org.eclipse.osgi.tests.eclipseadaptor;
 
-import junit.framework.Test;
-import junit.framework.TestSuite;
+import junit.framework.*;
 
 public class AllTests {
 	public static Test suite() {
 		TestSuite suite = new TestSuite(AllTests.class.getName());
 		suite.addTest(EnvironmentInfoTest.suite());
 		suite.addTest(FilePathTest.suite());
+		suite.addTest(new JUnit4TestAdapter(LocaleTransformationTest.class));
 		return suite;
 	}
 }
