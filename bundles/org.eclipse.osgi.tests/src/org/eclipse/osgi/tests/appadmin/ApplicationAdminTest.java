973cce5ef912b1f205c919ccc500a26684446578
Thomas Watson <tjwatson@us.ibm.com>
2014-10-31 11:05:35 -0500

    Bug 449465 - Compile error in org.eclipse.osgi.tests/plugin.xml

--- bundles/org.eclipse.osgi.tests/src/org/eclipse/osgi/tests/appadmin/ApplicationAdminTest.java
+++ bundles/org.eclipse.osgi.tests/src/org/eclipse/osgi/tests/appadmin/ApplicationAdminTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2013 IBM Corporation and others.
+ * Copyright (c) 2007, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -665,8 +665,12 @@ public class ApplicationAdminTest extends OSGiTest {
 		ArrayList instances = new ArrayList();
 		int i = 0;
 		try {
-			for (; i <= cardinality; i++)
+			for (; i <= cardinality; i++) {
 				instances.add(app.launch(null));
+				if (i == cardinality && hasMax) {
+					fail("Expected an ApplicationException.");
+				}
+			}
 		} catch (ApplicationException e) {
 			if (!hasMax || i != cardinality)
 				fail("Unexpected ApplicationException", e); //$NON-NLS-1$
@@ -680,7 +684,7 @@ public class ApplicationAdminTest extends OSGiTest {
 	}
 
 	public void testCardinality01() {
-		doTestCardinality01(PI_OSGI_TESTS + ".testCardinality01", 10, true); //$NON-NLS-1$
+		doTestCardinality01(PI_OSGI_TESTS + ".testCardinality01", 5, true); //$NON-NLS-1$
 	}
 
 	public void testCardinality02() {
