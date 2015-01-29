198c319cc09c485a91c93bbb2e47ba307c6afe89
Raymond Auge <raymond.auge@liferay.com>
2015-01-28 22:43:51 -0500

    Bug 458695 - create Jetty Service starter bundle
    Signed-off-by: Raymond Auge <raymond.auge@liferay.com>

---
+++ bundles/org.eclipse.equinox.http.jetty.starter/src/org/eclipse/equinox/http/jetty/starter/internal/Activator.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Raymond Auge and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Raymond Auge - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.equinox.http.jetty.starter.internal;
+
+import org.osgi.framework.*;
+import org.osgi.framework.startlevel.BundleStartLevel;
+
+public class Activator implements BundleActivator {
+
+	public void start(BundleContext context) throws Exception {
+		Bundle bundle = FrameworkUtil.getBundle(org.eclipse.equinox.http.jetty.JettyConstants.class);
+
+		BundleStartLevel bundleStartLevel = bundle.adapt(BundleStartLevel.class);
+
+		if (bundleStartLevel.isActivationPolicyUsed()) {
+			bundle.stop();
+			bundle.start();
+		}
+	}
+
+	public void stop(BundleContext context) throws Exception {
+		// ignore
+	}
+
+}
