955cbb81dd65c921de5121a14f1178c449e4636d
Pascal Rapicault <pascal@rapicorp.com>
2015-03-08 22:16:45 -0400

    Bug 461674 - [Mac] Look for eclipse.ini in new place
    Change-Id: I99a0b1ccfad3fe1b8374893fc0795b4ac1094b43
    Signed-off-by: Pascal Rapicault <pascal@rapicorp.com>

--- features/org.eclipse.equinox.executable.feature/library/eclipseConfig.c
+++ features/org.eclipse.equinox.executable.feature/library/eclipseConfig.c
@@ -13,6 +13,9 @@
 #include "eclipseOS.h"
 #include "eclipseConfig.h"
 
+#ifdef MACOSX
+#include <libgen.h>
+#endif
 #ifdef _WIN32
 
 #include <stdio.h>
@@ -76,6 +79,21 @@ _TCHAR* getIniFile(_TCHAR* program, int consoleLauncher){
 			}
 		}
 	}
+#elif MACOSX
+	//On MacOSX, the eclipse.ini is not a sibling of the executable.
+	//It is in ../Eclipse/<launcherName>.ini relatively to the executable.
+	char *dirc, *basec, *bname, *dname;
+	dirc = strdup(program);
+	basec = strdup(program);
+	dname = dirname(dirname(dirc));
+	bname = basename(basec);
+	config_file = realloc(config_file, strlen(dname) + strlen(bname) + 16 * sizeof(char));
+	sprintf(config_file, "%s/Eclipse/%s.ini", dname, bname);
+	printf("ini location is %s", config_file);
+	free(dirc);
+	free(basec);
+	free(dname);
+	free(bname);
 #else
 	/* Append the extension */
 	strcat(config_file, ".ini");
