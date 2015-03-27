c09b8738c4ba9f7313cc6b31fe464667ae5b90cb
Thomas Watson <tjwatson@us.ibm.com>
2015-03-27 07:58:19 -0500

    Bug 463273 - 3 compile warnings in official build: org.eclipse.equinox.ds.tests  - fix copyright dates

--- bundles/org.eclipse.equinox.ds.tests/bundles_src/tb7/org/eclipse/equinox/ds/tests/tb7/DynamicCircuit2.java
+++ bundles/org.eclipse.equinox.ds.tests/bundles_src/tb7/org/eclipse/equinox/ds/tests/tb7/DynamicCircuit2.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 1997-2015 by ProSyst Software GmbH
+ * Copyright (c) 1997-2009 by ProSyst Software GmbH
  * http://www.prosyst.com
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
