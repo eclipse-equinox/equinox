0cf5caa1400231038c44e978101f7c51517b5cd4
Marc-Andre Laperle <marc-andre.laperle@ericsson.com>
2015-05-02 00:02:23 -0400

    Bug 435742 - [GTK3] Some keybindings/shortcuts become unresponsive
    Add workaround in the Eclipse launcher for known issue in GTK3/Xinput2 - https://bugzilla.gnome.org/show_bug.cgi?id=677329
    
    Change-Id: I126ebdc7c0c876684125f939fae1d0ffcd27d456
    Signed-off-by: Marc-Andre Laperle <marc-andre.laperle@ericsson.com>

--- features/org.eclipse.equinox.executable.feature/library/gtk/eclipseGtkInit.c
+++ features/org.eclipse.equinox.executable.feature/library/gtk/eclipseGtkInit.c
@@ -125,6 +125,12 @@ int loadGtk() {
 	if (gtk3 == NULL || strcmp(gtk3,"1") == 0) {
 		gdkLib = dlopen(GDK3_LIB, DLFLAGS);
 		gtkLib = dlopen(GTK3_LIB, DLFLAGS);
+
+		/* Work around for https://bugzilla.gnome.org/show_bug.cgi?id=677329, see Eclipse bug 435742 */
+		char *gdkCoreDeviceEvents = getenv("GDK_CORE_DEVICE_EVENTS");
+		if (gdkCoreDeviceEvents == NULL) {
+			setenv("GDK_CORE_DEVICE_EVENTS", "1", 0);
+		}
 	}
 	if (!gtkLib || !gdkLib) { //if GTK+ 2
 		gdkLib = dlopen(GDK_LIB, DLFLAGS);
