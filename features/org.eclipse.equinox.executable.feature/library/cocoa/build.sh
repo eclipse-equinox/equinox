15d56969726892d8f960f4b0562575446c550957
Mikael Barbero <mikael@eclipse.org>
2015-09-25 17:40:16 +0200

    Bug 383545 - Rename library/carbon to library/cocoa
    Change-Id: Ibf3be14f750de816825000a2f1c8d17fbe8ed249
    Signed-off-by: Mikael Barbero <mikael@eclipse.org>

--- features/org.eclipse.equinox.executable.feature/library/cocoa/build.sh
+++ features/org.eclipse.equinox.executable.feature/library/cocoa/build.sh
@@ -3,9 +3,9 @@
 # Copyright (c) 2000, 2009 IBM Corporation and others.
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Eclipse Public License v1.0
-# which accompanies this distribution, and is available at 
+# which accompanies this distribution, and is available at
 # http://www.eclipse.org/legal/epl-v10.html
-# 
+#
 # Contributors:
 #     IBM Corporation - initial API and implementation
 #     Kevin Cornell (Rational Software Corporation)
@@ -27,12 +27,9 @@ cd `dirname $0`
 # Define default values for environment variables used in the makefiles.
 programOutput="eclipse"
 defaultOS="macosx"
-defaultOSArch="x86"
-defaultWS="carbon"
-makefile="make_carbon.mak"
-if [ "$OS" = "" ];  then
-    OS=`uname -s`
-fi
+defaultOSArch="x86_64"
+defaultWS="cocoa"
+makefile="make_cocoa.mak"
 
 # Parse the command line arguments and override the default values.
 extraArgs=""
@@ -55,46 +52,30 @@ while [ "$1" != "" ]; do
     shift
 done
 
+echo "build $defaultOSArch"
+
 # Set up environment variables needed by the makefiles.
 PROGRAM_OUTPUT="$programOutput"
 DEFAULT_OS="$defaultOS"
 DEFAULT_OS_ARCH="$defaultOSArch"
 DEFAULT_WS="$defaultWS"
 EXEC_DIR=../../../../../rt.equinox.binaries/org.eclipse.equinox.executable
-PPC_OUTPUT_DIR="$EXEC_DIR/bin/$defaultWS/$defaultOS/ppc/Eclipse.app/Contents/MacOS"
-X86_OUTPUT_DIR="$EXEC_DIR/bin/$defaultWS/$defaultOS/x86/Eclipse.app/Contents/MacOS"
-X86_64_OUTPUT_DIR="$EXEC_DIR/bin/$defaultWS/$defaultOS/x86_64/Eclipse.app/Contents/MacOS"
+PROGRAM_OUTPUT_DIR="$EXEC_DIR/bin/$defaultWS/$defaultOS/$defaultOSArch/Eclipse.app/Contents/MacOS"
 
-if [ "$DEFAULT_WS" == "cocoa" ]; then
-	makefile="make_cocoa.mak"
-	export MACOSX_DEPLOYMENT_TARGET=10.5
+# /System/Library/Frameworks/JavaVM.framework/Headers does not exist anymore on Yosemite
+if [ -e /System/Library/Frameworks/JavaVM.framework/Headers ]; then
+  JAVA_HEADERS="-I/System/Library/Frameworks/JavaVM.framework/Headers"
 else
-	export MACOSX_DEPLOYMENT_TARGET=10.3	
+  JAVA_HEADERS="-I$(/usr/libexec/java_home)/include -I$(/usr/libexec/java_home)/include/darwin"
 fi
 
-if [ "$DEFAULT_OS_ARCH" == "x86_64" ]; then
-	echo "build x86_64"
-	ARCHS="-arch x86_64"
-	PROGRAM_OUTPUT_DIR=$X86_64_OUTPUT_DIR
-	DEFAULT_OS_ARCH="x86_64"
-else
-	echo "build x86 and ppc"
-	ARCHS="-arch i386 -arch ppc"
-	PROGRAM_OUTPUT_DIR=$X86_OUTPUT_DIR
-fi
- 
-export PPC_OUTPUT_DIR X86_OUTPUT_DIR X86_64_OUTPUT_DIR PROGRAM_OUTPUT DEFAULT_OS DEFAULT_OS_ARCH DEFAULT_WS ARCHS PROGRAM_OUTPUT_DIR
+export PROGRAM_OUTPUT DEFAULT_OS DEFAULT_OS_ARCH DEFAULT_WS ARCHS PROGRAM_OUTPUT_DIR JAVA_HEADERS
 
-# If the OS is supported (a makefile exists)
-if [ "$makefile" != "" ]; then
-	if [ "$extraArgs" != "" ]; then
-		make -f $makefile $extraArgs
-	else
-		echo "Building $OS launcher. Defaults: -os $DEFAULT_OS -arch $DEFAULT_OS_ARCH -ws $DEFAULT_WS"
-		make -f $makefile clean
-		make -f $makefile all
-		make -f $makefile install
-	fi
+if [ "$extraArgs" != "" ]; then
+	make -f $makefile $extraArgs
 else
-	echo "Unknown OS ($OS) -- build aborted"
+	echo "Building $defaultOS launcher. Defaults: -os $DEFAULT_OS -arch $DEFAULT_OS_ARCH -ws $DEFAULT_WS"
+	make -f $makefile clean
+	make -f $makefile all
+	make -f $makefile install
 fi
--- features/org.eclipse.equinox.executable.feature/library/cocoa/make_cocoa.mak
+++ features/org.eclipse.equinox.executable.feature/library/cocoa/make_cocoa.mak
@@ -2,14 +2,14 @@
 # Copyright (c) 2000, 2008 IBM Corporation and others.
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Eclipse Public License v1.0
-# which accompanies this distribution, and is available at 
+# which accompanies this distribution, and is available at
 # http://www.eclipse.org/legal/epl-v10.html
-#   
-# Contributors: 
+#
+# Contributors:
 #     Kevin Cornell (Rational Software Corporation)
-#********************************************************************** 
+#**********************************************************************
 include ../make_version.mak
-# Makefile for creating the Carbon eclipse launcher program.
+# Makefile for creating the Cocoa eclipse launcher program.
 
 # This makefile expects the following environment variables set:
 #
@@ -27,21 +27,22 @@ PROGRAM_LIBRARY=eclipse_$(LIB_VERSION).so
 # Define the object modules to be compiled and flags.
 MAIN_OBJS = eclipseMain.o eclipseCarbonMain.o
 COMMON_OBJS = eclipseConfig.o eclipseCommon.o eclipseCarbonCommon.o
-DLL_OBJS	= eclipse.o eclipseCarbon.o eclipseUtil.o eclipseJNI.o eclipseShm.o NgImageData.o NgWinBMPFileFormat.o NgCommon.o
+DLL_OBJS	= eclipse.o eclipseCarbon.o eclipseUtil.o eclipseJNI.o eclipseShm.o
+#NgImageData.o NgWinBMPFileFormat.o NgCommon.o
 
 EXEC = $(PROGRAM_OUTPUT)
 DLL = $(PROGRAM_LIBRARY)
-LIBS = -framework Carbon
-ARCHS = -arch i386 -arch ppc
+LIBS = -framework Cocoa
+
 CFLAGS = -O -s \
-	-mmacosx-version-min=10.3 \
 	-Wall \
-	$(ARCHS) \
+	-DCOCOA -xobjective-c \
+  -arch x86_64 \
 	-DMACOSX \
 	-DDEFAULT_OS="\"$(DEFAULT_OS)\"" \
 	-DDEFAULT_OS_ARCH="\"$(DEFAULT_OS_ARCH)\"" \
 	-DDEFAULT_WS="\"$(DEFAULT_WS)\"" \
-	-I.. -I../motif -I/System/Library/Frameworks/JavaVM.framework/Headers
+	-I.. -I../motif $(JAVA_HEADERS)
 
 all: $(EXEC) $(DLL)
 
@@ -51,15 +52,15 @@ eclipse.o: ../eclipse.c ../eclipseOS.h ../eclipseCommon.h ../eclipseJNI.h
 eclipseCarbonMain.o : eclipseCarbonMain.c
 	$(CC) $(CFLAGS) -c eclipseCarbonMain.c -o $@
 
-eclipseMain.o: ../eclipseUnicode.h ../eclipseCommon.h eclipseMain.c ../eclipseMain.c 
+eclipseMain.o: ../eclipseUnicode.h ../eclipseCommon.h eclipseMain.c ../eclipseMain.c
 	$(CC) $(CFLAGS) -c eclipseMain.c -o $@
-	
+
 eclipseJNI.o: ../eclipseJNI.c ../eclipseCommon.h ../eclipseOS.h ../eclipseJNI.h
 	$(CC) $(CFLAGS) -c ../eclipseJNI.c -o $@
 
 eclipseUtil.o: ../eclipseUtil.c ../eclipseUtil.h ../eclipseOS.h
 	$(CC) $(CFLAGS) -c ../eclipseUtil.c -o $@
-	
+
 eclipseConfig.o: ../eclipseConfig.c ../eclipseConfig.h ../eclipseOS.h
 	$(CC) $(CFLAGS) -c ../eclipseConfig.c -o $@
 
@@ -85,8 +86,7 @@ $(DLL): $(DLL_OBJS) $(COMMON_OBJS)
 	$(CC) -bundle -o $(DLL) $(ARCHS) $(DLL_OBJS) $(COMMON_OBJS) $(LIBS)
 
 install: all
-	cp $(EXEC) $(PPC_OUTPUT_DIR)
-	cp $(EXEC) $(X86_OUTPUT_DIR)
+	cp -v $(EXEC) $(PROGRAM_OUTPUT_DIR)
 	rm -f $(EXEC) $(OBJS)
 
 clean:
