00920f9106e614eff9f89ff334ef0df1809fe88c
Thomas Watson <tjwatson@us.ibm.com>
2016-04-25 14:12:01 -0500

    Bug 490273 - Move tests to JUnit 4
    Revert back to junit 4 style.  Accidental usage of JUnit4TestAdapter came back.

--- bundles/org.eclipse.equinox.http.servlet.tests/src/org/eclipse/equinox/http/servlet/testbase/AllTests.java
+++ bundles/org.eclipse.equinox.http.servlet.tests/src/org/eclipse/equinox/http/servlet/testbase/AllTests.java
@@ -1,20 +1,23 @@
-
+/*******************************************************************************
+ * Copyright (c) 2016 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.equinox.http.servlet.testbase;
 
 import org.eclipse.equinox.http.servlet.tests.DispatchingTest;
 import org.eclipse.equinox.http.servlet.tests.ServletTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
 
-import junit.framework.JUnit4TestAdapter;
-import junit.framework.Test;
-import junit.framework.TestSuite;
-
-public class AllTests extends TestSuite {
-
-    public static Test suite() {
-        TestSuite suite = new TestSuite();
-        suite.addTest(new JUnit4TestAdapter(DispatchingTest.class));
-        suite.addTest(new JUnit4TestAdapter(ServletTest.class));
-        return suite;
-    }
-
+@RunWith(Suite.class)
+@SuiteClasses({DispatchingTest.class,ServletTest.class})
+public class AllTests {
+	// see @SuiteClasses
 }
