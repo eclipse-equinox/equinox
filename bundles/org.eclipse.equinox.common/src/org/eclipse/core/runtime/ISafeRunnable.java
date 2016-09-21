44d5102c882b80772a85adec0ca3effe7da5dc52
Sergey Prigogin <eclipse.sprigogin@gmail.com>
2016-09-21 15:11:14 -0700

    Bug 501778 - [common] Provide default implementation for ISafeRunnable.handleException(Throwable)
    Change-Id: I1f69f0701ad4034a878ecc241f867ca5078f9617
    Signed-off-by: Sergey Prigogin <eclipse.sprigogin@gmail.com>

--- bundles/org.eclipse.equinox.common/src/org/eclipse/core/runtime/ISafeRunnable.java
+++ bundles/org.eclipse.equinox.common/src/org/eclipse/core/runtime/ISafeRunnable.java
@@ -24,31 +24,32 @@ package org.eclipse.core.runtime;
  */
 public interface ISafeRunnable {
 	/**
-	 * Handles an exception thrown by this runnable's <code>run</code>
-	 * method.  The processing done here should be specific to the
-	 * particular usecase for this runnable.  Generalized exception
-	 * processing (e.g., logging in the platform's log) is done by the
-	 * {@link SafeRunner}.
-	 * <p>
-	 * All exceptions from the {@link #run} method are passed to this method,
-	 * along with certain {@link Error} types that are typically caused by
-	 * programming errors in the untrusted code being run.
-	 * </p>
-	 *
-	 * @param exception an exception which occurred during processing
-	 *		the body of this runnable (i.e., in <code>run()</code>)
-	 * @see SafeRunner#run(ISafeRunnable)
-	 */
-	public void handleException(Throwable exception);
-
-	/**
 	 * Runs this runnable.  Any exceptions thrown from this method will
-	 * be passed to this runnable's <code>handleException</code>
-	 * method.
+	 * be logged by the caller and passed to this runnable's
+	 * {@link #handleException} method.
 	 *
-	 * @exception Exception if a problem occurred while running this method.
-	 *		The exception will be processed by <code>handleException</code>
+	 * @exception Exception if a problem occurred while running this method
 	 * @see SafeRunner#run(ISafeRunnable)
 	 */
 	public void run() throws Exception;
+
+	/**
+	 * Handles an exception thrown by this runnable's {@link #run}
+	 * method.  The processing done here should be specific to the
+	 * particular use case for this runnable.  Generalized exception
+	 * processing, including logging in the Platform's log, is done by
+	 * the {@link SafeRunner}.
+	 * <p>
+	 * All exceptions from the {@link #run} method are passed to this
+	 * method, along with certain {@link Error} types that are typically
+	 * caused by programming errors in the untrusted code being run.
+	 * </p>
+	 *
+	 * @param exception an exception which occurred during processing
+	 *		the body of this runnable (i.e., in {@link #run})
+	 * @see SafeRunner#run(ISafeRunnable)
+	 */
+	public default void handleException(Throwable exception) {
+		// The exception has already been logged by the caller.
+	}
 }
