436f783a93cbe6440ceef7ad71c06e22c53d3af8
Thomas Watson <tjwatson@us.ibm.com>
2017-06-16 10:50:48 -0500

    Add api_filters for issues flagged in OSGi API
    Change-Id: I542dd7195b4156d856997d81780acd06006cc346
    Signed-off-by: Thomas Watson <tjwatson@us.ibm.com>

--- bundles/org.eclipse.osgi/supplement/src/org/eclipse/equinox/log/ExtendedLogService.java
+++ bundles/org.eclipse.osgi/supplement/src/org/eclipse/equinox/log/ExtendedLogService.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2011 IBM Corporation and others
+ * Copyright (c) 2006, 2017 IBM Corporation and others
  * All rights reserved. This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License v1.0 which
  * accompanies this distribution, and is available at
@@ -16,6 +16,7 @@ import org.osgi.service.log.LogService;
  * @ThreadSafe
  * @see Logger
  * @since 3.7
+ * @noimplement This interface is not intended to be implemented by clients.
  */
 public interface ExtendedLogService extends LogService, Logger {
 
