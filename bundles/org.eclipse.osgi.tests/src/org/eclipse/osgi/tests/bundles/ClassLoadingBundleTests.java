7e715abdab9180691657ceb0e0017ca86bdef937
Thomas Watson <tjwatson@us.ibm.com>
2017-09-18 08:02:03 -0500

    Bug 522430 - ClassLoadingBundleTests.testContextFinderEmptyGetResources is failing on ep48I-unit-cen64-gtk3
    Change test to use a different port from 8888

--- bundles/org.eclipse.osgi.tests/src/org/eclipse/osgi/tests/bundles/ClassLoadingBundleTests.java
+++ bundles/org.eclipse.osgi.tests/src/org/eclipse/osgi/tests/bundles/ClassLoadingBundleTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2016 IBM Corporation and others.
+ * Copyright (c) 2006, 2017 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -1739,7 +1739,7 @@ public class ClassLoadingBundleTests extends AbstractBundleTests {
 		try {
 			pool = Executors.newFixedThreadPool(3);
 
-			final String address = "http://localhost:8888/service";
+			final String address = "http://localhost:23512/service";
 
 			final WebService annotation = TestService.class.getAnnotation(WebService.class);
 			final String namespaceURI = annotation.serviceName();
