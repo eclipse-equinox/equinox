ff0b7576f6ffc9813721be9847d8c7b8d7eed1c1
Thomas Watson <tjwatson@us.ibm.com>
2018-08-15 07:55:40 -0500

    Bug 537702 - Expose Nested Dir Name as part of NestedDirBundleFile
    Change-Id: Id3c8be4529c76ea5bc26ead6d50917ce0a471c20
    Signed-off-by: Thomas Watson <tjwatson@us.ibm.com>

--- bundles/org.eclipse.osgi/container/src/org/eclipse/osgi/storage/bundlefile/NestedDirBundleFile.java
+++ bundles/org.eclipse.osgi/container/src/org/eclipse/osgi/storage/bundlefile/NestedDirBundleFile.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2013 IBM Corporation and others.
+ * Copyright (c) 2005, 2018 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -138,4 +138,10 @@ public class NestedDirBundleFile extends BundleFile {
 	public void open() throws IOException {
 		// do nothing
 	}
+
+	@Override
+	public String toString() {
+		return super.toString() + '[' + nestedDirName + ']';
+	}
+
 }
