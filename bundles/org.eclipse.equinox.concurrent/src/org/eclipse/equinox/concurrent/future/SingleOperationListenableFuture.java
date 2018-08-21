a05e887ec911edc1bdde68182cf6a2dbad65915e
Lars Vogel <Lars.Vogel@vogella.com>
2018-08-21 17:28:48 +0200

    Bug 535802 - EPL-2.0 for plug-ins in equinox.bundles
    Script used for this update:
    
    git checkout master
    git reset --hard origin/master
    git pull
    
    find . -type f ! -name "*.exsd" ! -name "org.eclipse.jdt.ui.prefs" -exec
    sed -i  's/Eclipse Public License v1.0/Eclipse Public License 2.0/g' {}
    +
    
    find . -type f  ! -name "*.exsd" ! -name "org.eclipse.jdt.ui.prefs"
    -exec sed -i -E
    's/([[:blank:]]*[^[:blank:]])*([[:blank:]]*)http:\/\/www.eclipse.org\/legal\/epl-v10.html/\1\2https:\/\/www.eclipse.org\/legal\/epl-2.0\/\
    \1\
    \1\2SPDX-License-Identifier: EPL-2.0/' {} +
    
    
    # Get rid of the All rights reserved
    find . -type f ! -name "*.exsd" ! -name "org.eclipse.jdt.ui.prefs" !
    -name "pom.xml" -exec sed -i -E
    's/([[:blank:]]*[^[:blank:]])*([[:blank:]]*)(All rights reserved.
    )/\1\n\1\2/' {} +
    
    Change-Id: I4a3195578bdf2d2c8ae86224e6156efcf096362f
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- bundles/org.eclipse.equinox.concurrent/src/org/eclipse/equinox/concurrent/future/SingleOperationListenableFuture.java
+++ bundles/org.eclipse.equinox.concurrent/src/org/eclipse/equinox/concurrent/future/SingleOperationListenableFuture.java
@@ -1,8 +1,11 @@
 /******************************************************************************
- * Copyright (c) 2010, 2013 Composent, Inc. and others. All rights reserved. This
+.
+. This
  * program and the accompanying materials are made available under the terms of
- * the Eclipse Public License v1.0 which accompanies this distribution, and is
- * available at http://www.eclipse.org/legal/epl-v10.html
+ * the Eclipse Public License 2.0 which accompanies this distribution, and is
+t https://www.eclipse.org/legal/epl-2.0/
+t
+t SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
  *   Composent, Inc. - initial API and implementation
