b45028c20bb3b592527099680a55c76fa66c94f3
Lars Vogel <Lars.Vogel@vogella.com>
2018-08-22 17:04:46 +0200

    Adds missing line break before link to EPL 1.0
    Change-Id: Ia1955ae095634d208ec4184483515b72edc5e506
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- bundles/org.eclipse.osgi.tests/src/org/eclipse/equinox/log/test/TestListener.java
+++ bundles/org.eclipse.osgi.tests/src/org/eclipse/equinox/log/test/TestListener.java
@@ -2,7 +2,8 @@
  * Copyright (c) 2007, 2018 IBM Corporation All rights reserved. This program
  * and the accompanying materials are made available under the terms of the
  * Eclipse Public License v1.0 which accompanies this distribution, and is
- * available at http://www.eclipse.org/legal/epl-v10.html
+ * available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *******************************************************************************/
 package org.eclipse.equinox.log.test;
 
