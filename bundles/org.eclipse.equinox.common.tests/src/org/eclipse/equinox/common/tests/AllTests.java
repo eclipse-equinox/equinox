036a8c573d1e3fdceab9d026494a3c1e6ac0c846
Julian Honnen <julian.honnen@vector.com>
2019-02-06 16:56:59 +0100

    Bug 263880 - moved equinox tests from core.tests.runtime
    Created new common test bundle for tests of Equinox components from
    org.eclipse.core.tests.runtime.
    
    Change-Id: Ic838b8038322940051701c4d1e270ad75e9a8470
    Signed-off-by: Julian Honnen <julian.honnen@vector.com>

---
+++ bundles/org.eclipse.equinox.common.tests/src/org/eclipse/equinox/common/tests/AllTests.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 1997-2009 by ProSyst Software GmbH
+ * http://www.prosyst.com
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *    ProSyst Software GmbH - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.equinox.common.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses(StatusTest.class)
+public class AllTests {
+	// intentionally left blank
+}
