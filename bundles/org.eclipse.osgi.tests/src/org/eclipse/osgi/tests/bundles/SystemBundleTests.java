8706b4088137e0949adeb3fbd527167a9aea266f
Thomas Watson <tjwatson@us.ibm.com>
2019-07-17 10:29:56 -0500

    Update to Java 8 required execution environment
    Change-Id: I8b31902ac6e006873dfe5ad4a77698dd2328e645
    Signed-off-by: Thomas Watson <tjwatson@us.ibm.com>

--- bundles/org.eclipse.osgi.tests/src/org/eclipse/osgi/tests/bundles/SystemBundleTests.java
+++ bundles/org.eclipse.osgi.tests/src/org/eclipse/osgi/tests/bundles/SystemBundleTests.java
@@ -1020,25 +1020,25 @@ public class SystemBundleTests extends AbstractBundleTests {
 	}
 
 	public void testChangeEE() throws IOException, BundleException {
-		URL javaSE7Profile = OSGiTestsActivator.getContext().getBundle(Constants.SYSTEM_BUNDLE_LOCATION).getEntry("JavaSE-1.7.profile");
 		URL javaSE8Profile = OSGiTestsActivator.getContext().getBundle(Constants.SYSTEM_BUNDLE_LOCATION).getEntry("JavaSE-1.8.profile");
+		URL javaSE9Profile = OSGiTestsActivator.getContext().getBundle(Constants.SYSTEM_BUNDLE_LOCATION).getEntry("JavaSE-9.profile");
 
-		// configure equinox for javaSE 8
+		// configure equinox for javaSE 9
 		File config = OSGiTestsActivator.getContext().getDataFile(getName()); //$NON-NLS-1$
 		Map<String, Object> configuration = new HashMap<String, Object>();
 		configuration.put(Constants.FRAMEWORK_STORAGE, config.getAbsolutePath());
-		configuration.put("osgi.java.profile", javaSE8Profile.toExternalForm()); //$NON-NLS-1$
+		configuration.put("osgi.java.profile", javaSE9Profile.toExternalForm()); //$NON-NLS-1$
 
 		Equinox equinox = new Equinox(configuration);
 		equinox.start();
 
-		// install a bundle that requires java 8
+		// install a bundle that requires java 9
 		BundleContext systemContext = equinox.getBundleContext();
 		assertNotNull("System context is null", systemContext); //$NON-NLS-1$
 		Map<String, String> testHeaders = new HashMap<String, String>();
 		testHeaders.put(Constants.BUNDLE_MANIFESTVERSION, "2");
 		testHeaders.put(Constants.BUNDLE_SYMBOLICNAME, getName());
-		testHeaders.put(Constants.BUNDLE_REQUIREDEXECUTIONENVIRONMENT, "JavaSE-1.8");
+		testHeaders.put(Constants.BUNDLE_REQUIREDEXECUTIONENVIRONMENT, "JavaSE-9");
 		File testBundle = createBundle(config, getName(), testHeaders);
 		Bundle b = systemContext.installBundle("reference:file:///" + testBundle.getAbsolutePath()); //$NON-NLS-1$
 		long bid = b.getBundleId();
@@ -1054,8 +1054,8 @@ public class SystemBundleTests extends AbstractBundleTests {
 			fail("Unexpected interrupted exception", e); //$NON-NLS-1$
 		}
 
-		// configure equinox for java 7
-		configuration.put("osgi.java.profile", javaSE7Profile.toExternalForm());
+		// configure equinox for java 8
+		configuration.put("osgi.java.profile", javaSE8Profile.toExternalForm());
 		equinox = new Equinox(configuration);
 		try {
 			equinox.start();
@@ -1074,8 +1074,8 @@ public class SystemBundleTests extends AbstractBundleTests {
 			fail("Unexpected interrupted exception", e); //$NON-NLS-1$
 		}
 
-		// move back to java 8
-		configuration.put("osgi.java.profile", javaSE8Profile.toExternalForm());
+		// move back to java 9
+		configuration.put("osgi.java.profile", javaSE9Profile.toExternalForm());
 		equinox = new Equinox(configuration);
 		try {
 			equinox.start();
@@ -1136,7 +1136,7 @@ public class SystemBundleTests extends AbstractBundleTests {
 		for (File testBundle : testBundles) {
 			try {
 				systemContext.installBundle("reference:file:///" + testBundle.getAbsolutePath()); //$NON-NLS-1$
-			}catch (BundleException e) {
+			} catch (BundleException e) {
 				fail("Unexpected install error", e); //$NON-NLS-1$
 			}
 		}
@@ -2690,7 +2690,7 @@ public class SystemBundleTests extends AbstractBundleTests {
 		config.mkdirs();
 		Map<String, Object> configuration = new HashMap<String, Object>();
 		configuration.put(Constants.FRAMEWORK_STORAGE, config.getAbsolutePath());
-		configuration.put(Constants.FRAMEWORK_SYSTEMCAPABILITIES, "osgi.ee; osgi.ee=JavaSE; version:Version=1.7, something.system");
+		configuration.put(Constants.FRAMEWORK_SYSTEMCAPABILITIES, "osgi.ee; osgi.ee=JavaSE; version:Version=1.8, something.system");
 		configuration.put(Constants.FRAMEWORK_SYSTEMPACKAGES, "something.system");
 		configuration.put(Constants.FRAMEWORK_SYSTEMCAPABILITIES_EXTRA, "something.extra");
 		configuration.put(Constants.FRAMEWORK_SYSTEMPACKAGES_EXTRA, "something.extra");
@@ -2769,7 +2769,6 @@ public class SystemBundleTests extends AbstractBundleTests {
 			doTestJavaProfile("1.10.1", "JavaSE-1.8", null);
 			doTestJavaProfile("1.9", "JavaSE-1.8", null);
 			doTestJavaProfile("1.8", "JavaSE-1.8", null);
-			doTestJavaProfile("1.7", "JavaSE-1.7", null);
 			doTestJavaProfile("1.8", "JavaSE/compact3-1.8", "compact3");
 			doTestJavaProfile("1.8", "JavaSE/compact3-1.8", "\"compact3\"");
 			doTestJavaProfile("1.8", "JavaSE/compact3-1.8", " \"compact3\" ");
