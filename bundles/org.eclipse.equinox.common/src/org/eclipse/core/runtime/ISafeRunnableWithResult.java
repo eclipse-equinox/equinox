1cf8213ddf6fefa2cf8bd877ea0fbe45f78428c0
Alexander Kurtakov <akurtako@redhat.com>
2020-02-06 10:17:00 +0200

    Bug 558642 - SafeRunner should be able to return results
    Fix unmappable UTF-8 character breaking javadoc build.
    
    Change-Id: If2a442cec56e5fea8d435bbe7ca1b13d4928d71b
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- bundles/org.eclipse.equinox.common/src/org/eclipse/core/runtime/ISafeRunnableWithResult.java
+++ bundles/org.eclipse.equinox.common/src/org/eclipse/core/runtime/ISafeRunnableWithResult.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2020 Marcus Höpfner and others.
+ * Copyright (c) 2020 Marcus Hoepfner and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -9,7 +9,7 @@
  * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
- *     Marcus Höpfner - initial API and implementation
+ *     Marcus Hoepfner - initial API and implementation
  *******************************************************************************/
 package org.eclipse.core.runtime;
 
