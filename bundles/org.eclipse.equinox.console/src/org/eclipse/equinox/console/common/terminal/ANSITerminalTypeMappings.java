045e3eed3bc148aff34d335657c91f3f65ad1e28
Alexander Kurtakov <akurtako@redhat.com>
2020-05-25 09:09:04 +0300

    Bug 563467 - Deploy ecj compiler from 4.16 M3 and use it for 4.16 RC builds
    Fix needless import in equinox.console
    
    Change-Id: I8ddb7e4eead2b11f31d841517b6feaae79c57eba
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- bundles/org.eclipse.equinox.console/src/org/eclipse/equinox/console/common/terminal/ANSITerminalTypeMappings.java
+++ bundles/org.eclipse.equinox.console/src/org/eclipse/equinox/console/common/terminal/ANSITerminalTypeMappings.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2017 SAP AG and others.
+ * Copyright (c) 2010, 2020 SAP AG and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
