ea66e068b5d29ef6a06014a6e457e3b4508925e8
Alexander Kurtakov <akurtako@redhat.com>
2020-06-12 11:00:45 +0300

    Fix _ used as identifier giving warning on Java 8
    Change-Id: I975d6e009a8debfe8070689ba55c0e87f7b3821e
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- bundles/org.eclipse.equinox.region.tests/src/org/eclipse/equinox/region/tests/system/RegionSystemTests.java
+++ bundles/org.eclipse.equinox.region.tests/src/org/eclipse/equinox/region/tests/system/RegionSystemTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2015 IBM Corporation and others.
+ * Copyright (c) 2011, 2020 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -677,7 +677,7 @@ public class RegionSystemTests extends AbstractRegionSystemTest {
 		try {
 			digraph.setDefaultRegion(pp1Region);
 			assertFalse("IllegalArgumentException not thrown for setting non-existing region as default", true);
-		} catch (IllegalStateException _) {
+		} catch (IllegalStateException e) {
 			assertNull("DefaultRegion is not null", digraph.getDefaultRegion());
 		}
 	}
