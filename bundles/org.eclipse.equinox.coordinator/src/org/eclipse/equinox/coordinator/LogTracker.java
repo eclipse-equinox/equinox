c027995bee75525b757df31bb1f85b40db37d52c
Torbjörn SVENSSON <torbjorn.svensson@st.com>
2023-10-01 23:05:08 +0200

    Format java files in o.e.equinox.coordinator
    This was achieved by running:
    eclipse -consolelog -nosplash -application org.eclipse.jdt.core.JavaCodeFormatter \
      -config .settings/org.eclipse.jdt.core.prefs . -data `mktemp -d`
    
    Signed-off-by: Torbjörn SVENSSON <torbjorn.svensson@st.com>

--- bundles/org.eclipse.equinox.coordinator/src/org/eclipse/equinox/coordinator/LogTracker.java
+++ bundles/org.eclipse.equinox.coordinator/src/org/eclipse/equinox/coordinator/LogTracker.java
@@ -23,8 +23,8 @@ import org.osgi.service.log.LogService;
 import org.osgi.util.tracker.ServiceTracker;
 
 /**
- * LogTracker class. This class encapsulates the LogService
- * and handles all issues such as the service coming and going.
+ * LogTracker class. This class encapsulates the LogService and handles all
+ * issues such as the service coming and going.
  */
 
 public class LogTracker extends ServiceTracker<LogService, LogService> {
@@ -38,7 +38,7 @@ public class LogTracker extends ServiceTracker<LogService, LogService> {
 	 * Create new LogTracker.
 	 *
 	 * @param context BundleContext of parent bundle.
-	 * @param out Default PrintStream to use if LogService is unavailable.
+	 * @param out     Default PrintStream to use if LogService is unavailable.
 	 */
 	public LogTracker(BundleContext context, PrintStream out) {
 		super(context, clazz, null);
@@ -47,7 +47,7 @@ public class LogTracker extends ServiceTracker<LogService, LogService> {
 
 	/*
 	 * ----------------------------------------------------------------------
-	 *      LogService Interface implementation
+	 * LogService Interface implementation
 	 * ----------------------------------------------------------------------
 	 */
 
@@ -90,8 +90,8 @@ public class LogTracker extends ServiceTracker<LogService, LogService> {
 	/**
 	 * The LogService is not available so we write the message to a PrintStream.
 	 *
-	 * @param level Logging level
-	 * @param message Log message.
+	 * @param level     Logging level
+	 * @param message   Log message.
 	 * @param throwable Log exception or null if none.
 	 * @param reference ServiceReference associated with message or null if none.
 	 */
@@ -99,39 +99,39 @@ public class LogTracker extends ServiceTracker<LogService, LogService> {
 	protected void noLogService(int level, String message, Throwable throwable, ServiceReference<?> reference) {
 		if (out != null) {
 			synchronized (out) {
-				// Bug #113286.  If no log service present and messages are being
+				// Bug #113286. If no log service present and messages are being
 				// printed to stdout, prepend message with a timestamp.
 				String timestamp = getDate(new Date());
 				out.print(timestamp + " "); //$NON-NLS-1$
 
 				switch (level) {
-					case LogService.LOG_DEBUG : {
-						out.print(LogTrackerMsg.Debug);
+				case LogService.LOG_DEBUG: {
+					out.print(LogTrackerMsg.Debug);
 
-						break;
-					}
-					case LogService.LOG_INFO : {
-						out.print(LogTrackerMsg.Info);
+					break;
+				}
+				case LogService.LOG_INFO: {
+					out.print(LogTrackerMsg.Info);
 
-						break;
-					}
-					case LogService.LOG_WARNING : {
-						out.print(LogTrackerMsg.Warning);
+					break;
+				}
+				case LogService.LOG_WARNING: {
+					out.print(LogTrackerMsg.Warning);
 
-						break;
-					}
-					case LogService.LOG_ERROR : {
-						out.print(LogTrackerMsg.Error);
+					break;
+				}
+				case LogService.LOG_ERROR: {
+					out.print(LogTrackerMsg.Error);
 
-						break;
-					}
-					default : {
-						out.print("["); //$NON-NLS-1$
-						out.print(LogTrackerMsg.Unknown_Log_level);
-						out.print("]: "); //$NON-NLS-1$
+					break;
+				}
+				default: {
+					out.print("["); //$NON-NLS-1$
+					out.print(LogTrackerMsg.Unknown_Log_level);
+					out.print("]: "); //$NON-NLS-1$
 
-						break;
-					}
+					break;
+				}
 				}
 
 				out.println(message);
