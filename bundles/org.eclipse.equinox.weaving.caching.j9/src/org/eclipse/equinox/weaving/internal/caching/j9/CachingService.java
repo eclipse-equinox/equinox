9ed914e4b428caaabecaf54e4199c20c6c00f497
Torbjörn SVENSSON <torbjorn.svensson@st.com>
2023-10-01 23:07:24 +0200

    Format java files in o.e.equinox.weaving.caching.j9
    This was achieved by running:
    eclipse -consolelog -nosplash -application org.eclipse.jdt.core.JavaCodeFormatter \
      -config .settings/org.eclipse.jdt.core.prefs . -data `mktemp -d`
    
    Signed-off-by: Torbjörn SVENSSON <torbjorn.svensson@st.com>

--- bundles/org.eclipse.equinox.weaving.caching.j9/src/org/eclipse/equinox/weaving/internal/caching/j9/CachingService.java
+++ bundles/org.eclipse.equinox.weaving.caching.j9/src/org/eclipse/equinox/weaving/internal/caching/j9/CachingService.java
@@ -32,121 +32,110 @@ import org.osgi.framework.Bundle;
 
 public class CachingService implements ICachingService {
 
-    SharedClassURLHelper urlhelper;
+	SharedClassURLHelper urlhelper;
 
-    private final Bundle bundle;
+	private final Bundle bundle;
 
-    private final ClassLoader classLoader;
+	private final ClassLoader classLoader;
 
-    private final String partition;
+	private final String partition;
 
-    public CachingService(final ClassLoader loader, final Bundle bundle,
-            final String key) {
-        if (CachingServicePlugin.DEBUG)
-            System.out.println("> CachingService.<init>() bundle="
-                    + bundle.getSymbolicName() + ", loader=" + loader
-                    + ", key='" + key + "'");
-        this.bundle = bundle;
-        this.classLoader = loader;
-        this.partition = hashNamespace(key + bundle.getBundleId()
-                + bundle.getLastModified());
-        try {
-            urlhelper = Shared.getSharedClassHelperFactory().getURLHelper(
-                    classLoader);
-        } catch (final HelperAlreadyDefinedException e) {
-            e.printStackTrace();
-        }
-        if (CachingServicePlugin.DEBUG)
-            System.out.println("< CachingService.<init>() partition='"
-                    + partition + "', urlhelper=" + urlhelper);
-    }
+	public CachingService(final ClassLoader loader, final Bundle bundle, final String key) {
+		if (CachingServicePlugin.DEBUG)
+			System.out.println("> CachingService.<init>() bundle=" + bundle.getSymbolicName() + ", loader=" + loader
+					+ ", key='" + key + "'");
+		this.bundle = bundle;
+		this.classLoader = loader;
+		this.partition = hashNamespace(key + bundle.getBundleId() + bundle.getLastModified());
+		try {
+			urlhelper = Shared.getSharedClassHelperFactory().getURLHelper(classLoader);
+		} catch (final HelperAlreadyDefinedException e) {
+			e.printStackTrace();
+		}
+		if (CachingServicePlugin.DEBUG)
+			System.out.println("< CachingService.<init>() partition='" + partition + "', urlhelper=" + urlhelper);
+	}
 
-    /**
-     * @see org.eclipse.equinox.service.weaving.ICachingService#canCacheGeneratedClasses()
-     */
-    public boolean canCacheGeneratedClasses() {
-        return false;
-    }
+	/**
+	 * @see org.eclipse.equinox.service.weaving.ICachingService#canCacheGeneratedClasses()
+	 */
+	public boolean canCacheGeneratedClasses() {
+		return false;
+	}
 
-    /**
-     * @see org.eclipse.equinox.service.weaving.ICachingService#findStoredClass(java.lang.String,
-     *      java.net.URL, java.lang.String)
-     */
-    public CacheEntry findStoredClass(final String namespace,
-            final URL sourceFileURL, final String name) {
-        final byte[] bytes = urlhelper.findSharedClass(partition,
-                sourceFileURL, name);
-        if (CachingServicePlugin.DEBUG && bytes != null)
-            System.out.println("- CachingService.findStoredClass() bundle="
-                    + bundle.getSymbolicName() + ", name=" + name + ", url="
-                    + sourceFileURL + ", bytes=" + bytes);
+	/**
+	 * @see org.eclipse.equinox.service.weaving.ICachingService#findStoredClass(java.lang.String,
+	 *      java.net.URL, java.lang.String)
+	 */
+	public CacheEntry findStoredClass(final String namespace, final URL sourceFileURL, final String name) {
+		final byte[] bytes = urlhelper.findSharedClass(partition, sourceFileURL, name);
+		if (CachingServicePlugin.DEBUG && bytes != null)
+			System.out.println("- CachingService.findStoredClass() bundle=" + bundle.getSymbolicName() + ", name="
+					+ name + ", url=" + sourceFileURL + ", bytes=" + bytes);
 
-        if (bytes != null) {
-            return new CacheEntry(true, bytes);
-        } else {
-            return new CacheEntry(false, null);
-        }
-    }
+		if (bytes != null) {
+			return new CacheEntry(true, bytes);
+		} else {
+			return new CacheEntry(false, null);
+		}
+	}
 
-    /**
-     * Hash the shared class namespace using MD5
-     * 
-     * @param keyToHash
-     * @return the MD5 version of the input string
-     */
-    public String hashNamespace(final String namespace) {
-        MessageDigest md = null;
-        try {
-            md = MessageDigest.getInstance("MD5");
-        } catch (final NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-        final StringBuffer result = new StringBuffer();
-        for (final byte b : md.digest(namespace.getBytes())) {
-            int num;
-            if (b < 0) {
-                num = b + 256;
-            } else {
-                num = b;
-            }
-            String s = Integer.toHexString(num);
-            while (s.length() < 2) {
-                s = "0" + s;
-            }
-            result.append(s);
-        }
-        return new String(result);
-    }
+	/**
+	 * Hash the shared class namespace using MD5
+	 * 
+	 * @param keyToHash
+	 * @return the MD5 version of the input string
+	 */
+	public String hashNamespace(final String namespace) {
+		MessageDigest md = null;
+		try {
+			md = MessageDigest.getInstance("MD5");
+		} catch (final NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		final StringBuffer result = new StringBuffer();
+		for (final byte b : md.digest(namespace.getBytes())) {
+			int num;
+			if (b < 0) {
+				num = b + 256;
+			} else {
+				num = b;
+			}
+			String s = Integer.toHexString(num);
+			while (s.length() < 2) {
+				s = "0" + s;
+			}
+			result.append(s);
+		}
+		return new String(result);
+	}
 
-    /**
-     * @see org.eclipse.equinox.service.weaving.ICachingService#stop()
-     */
-    public void stop() {
-    }
+	/**
+	 * @see org.eclipse.equinox.service.weaving.ICachingService#stop()
+	 */
+	public void stop() {
+	}
 
-    /**
-     * @see org.eclipse.equinox.service.weaving.ICachingService#storeClass(java.lang.String,
-     *      java.net.URL, java.lang.Class, byte[])
-     */
-    public boolean storeClass(final String namespace, final URL sourceFileURL,
-            final Class<?> clazz, final byte[] classbytes) {
-        final boolean success = urlhelper.storeSharedClass(partition,
-                sourceFileURL, clazz);
-        if (CachingServicePlugin.DEBUG && success)
-            System.out.println("- CachingService.storeClass() bundle="
-                    + bundle.getSymbolicName() + ", clazz=" + clazz + ", url="
-                    + sourceFileURL);
-        return success;
-    }
+	/**
+	 * @see org.eclipse.equinox.service.weaving.ICachingService#storeClass(java.lang.String,
+	 *      java.net.URL, java.lang.Class, byte[])
+	 */
+	public boolean storeClass(final String namespace, final URL sourceFileURL, final Class<?> clazz,
+			final byte[] classbytes) {
+		final boolean success = urlhelper.storeSharedClass(partition, sourceFileURL, clazz);
+		if (CachingServicePlugin.DEBUG && success)
+			System.out.println("- CachingService.storeClass() bundle=" + bundle.getSymbolicName() + ", clazz=" + clazz
+					+ ", url=" + sourceFileURL);
+		return success;
+	}
 
-    /**
-     * @see org.eclipse.equinox.service.weaving.ICachingService#storeClassAndGeneratedClasses(java.lang.String,
-     *      java.net.URL, java.lang.Class, byte[], java.util.Map)
-     */
-    public boolean storeClassAndGeneratedClasses(final String namespace,
-            final URL sourceFileUrl, final Class<?> clazz,
-            final byte[] classbytes, final Map<String, byte[]> generatedClasses) {
-        return false;
-    }
+	/**
+	 * @see org.eclipse.equinox.service.weaving.ICachingService#storeClassAndGeneratedClasses(java.lang.String,
+	 *      java.net.URL, java.lang.Class, byte[], java.util.Map)
+	 */
+	public boolean storeClassAndGeneratedClasses(final String namespace, final URL sourceFileUrl, final Class<?> clazz,
+			final byte[] classbytes, final Map<String, byte[]> generatedClasses) {
+		return false;
+	}
 
 }
--- bundles/org.eclipse.equinox.weaving.caching.j9/src/org/eclipse/equinox/weaving/internal/caching/j9/CachingServicePlugin.java
+++ bundles/org.eclipse.equinox.weaving.caching.j9/src/org/eclipse/equinox/weaving/internal/caching/j9/CachingServicePlugin.java
@@ -30,99 +30,90 @@ import org.osgi.framework.ServiceReference;
  */
 public class CachingServicePlugin implements BundleActivator {
 
-    public static boolean DEBUG;
+	public static boolean DEBUG;
 
-    public static boolean verbose = Boolean
-            .getBoolean("org.aspectj.osgi.verbose");
+	public static boolean verbose = Boolean.getBoolean("org.aspectj.osgi.verbose");
 
-    /**
-     * The constructor.
-     */
-    public CachingServicePlugin() {
-    }
+	/**
+	 * The constructor.
+	 */
+	public CachingServicePlugin() {
+	}
 
-    /**
-     * This method is called upon plug-in activation
-     */
-    public void start(final BundleContext context) throws Exception {
-        if (CachingServicePlugin.DEBUG)
-            System.out.println("> CachingServicePlugin.start() context="
-                    + context);
+	/**
+	 * This method is called upon plug-in activation
+	 */
+	public void start(final BundleContext context) throws Exception {
+		if (CachingServicePlugin.DEBUG)
+			System.out.println("> CachingServicePlugin.start() context=" + context);
 
-        loadOptions(context);
+		loadOptions(context);
 
-        //are we on J9?
-        if (shouldRegister()) {
-            if (verbose)
-                System.err
-                        .println("[org.eclipse.equinox.weaving.caching.j9] info starting J9 caching service ...");
-            final String name = ICachingServiceFactory.class.getName();
-            final CachingServiceFactory cachingServiceFactory = new CachingServiceFactory();
-            context.registerService(name, cachingServiceFactory, null);
-        } else {
-            if (verbose)
-                System.err
-                        .println("[org.eclipse.equinox.weaving.caching.j9] warning cannot start J9 caching service");
-        }
+		// are we on J9?
+		if (shouldRegister()) {
+			if (verbose)
+				System.err.println("[org.eclipse.equinox.weaving.caching.j9] info starting J9 caching service ...");
+			final String name = ICachingServiceFactory.class.getName();
+			final CachingServiceFactory cachingServiceFactory = new CachingServiceFactory();
+			context.registerService(name, cachingServiceFactory, null);
+		} else {
+			if (verbose)
+				System.err.println("[org.eclipse.equinox.weaving.caching.j9] warning cannot start J9 caching service");
+		}
 
-        if (CachingServicePlugin.DEBUG)
-            System.out.println("< CachingServicePlugin.start()");
-    }
+		if (CachingServicePlugin.DEBUG)
+			System.out.println("< CachingServicePlugin.start()");
+	}
 
-    /**
-     * This method is called when the plug-in is stopped
-     */
-    public void stop(final BundleContext context) throws Exception {
-    }
+	/**
+	 * This method is called when the plug-in is stopped
+	 */
+	public void stop(final BundleContext context) throws Exception {
+	}
 
-    private void loadOptions(final BundleContext context) {
-        // all this is only to get the application args		
-        DebugOptions service = null;
-        final ServiceReference<?> reference = context
-                .getServiceReference(DebugOptions.class.getName());
-        if (reference != null)
-            service = (DebugOptions) context.getService(reference);
-        if (service == null) return;
-        try {
-            DEBUG = service.getBooleanOption(
-                    "org.aspectj.osgi.service.caching.j9/debug", false);
-        } finally {
-            // we have what we want - release the service
-            context.ungetService(reference);
-        }
-    }
+	private void loadOptions(final BundleContext context) {
+		// all this is only to get the application args
+		DebugOptions service = null;
+		final ServiceReference<?> reference = context.getServiceReference(DebugOptions.class.getName());
+		if (reference != null)
+			service = (DebugOptions) context.getService(reference);
+		if (service == null)
+			return;
+		try {
+			DEBUG = service.getBooleanOption("org.aspectj.osgi.service.caching.j9/debug", false);
+		} finally {
+			// we have what we want - release the service
+			context.ungetService(reference);
+		}
+	}
 
-    private boolean shouldRegister() {
-        if (CachingServicePlugin.DEBUG)
-            System.out.println("> CachingServicePlugin.shouldRegister()");
+	private boolean shouldRegister() {
+		if (CachingServicePlugin.DEBUG)
+			System.out.println("> CachingServicePlugin.shouldRegister()");
 
-        boolean enabled;
-        try {
-            Class.forName("com.ibm.oti.vm.VM"); //if this fails we are not on J9
-            final boolean sharing = Shared.isSharingEnabled(); //if not using shared classes we want a different adaptor
-            if (CachingServicePlugin.DEBUG)
-                System.out
-                        .println("- CachingServicePlugin.shouldRegister() sharing="
-                                + sharing);
+		boolean enabled;
+		try {
+			Class.forName("com.ibm.oti.vm.VM"); // if this fails we are not on J9
+			final boolean sharing = Shared.isSharingEnabled(); // if not using shared classes we want a different
+																// adaptor
+			if (CachingServicePlugin.DEBUG)
+				System.out.println("- CachingServicePlugin.shouldRegister() sharing=" + sharing);
 
-            if (sharing) {
-                enabled = true;
-            } else {
-                enabled = false;
-            }
-        } catch (final ClassNotFoundException ex) {
-            if (CachingServicePlugin.DEBUG)
-                System.out
-                        .println("E CachingServicePlugin.shouldRegister() ex="
-                                + ex);
-            //not on J9
-            enabled = false;
-        }
+			if (sharing) {
+				enabled = true;
+			} else {
+				enabled = false;
+			}
+		} catch (final ClassNotFoundException ex) {
+			if (CachingServicePlugin.DEBUG)
+				System.out.println("E CachingServicePlugin.shouldRegister() ex=" + ex);
+			// not on J9
+			enabled = false;
+		}
 
-        if (CachingServicePlugin.DEBUG)
-            System.out.println("< CachingServicePlugin.shouldRegister() "
-                    + enabled);
-        return enabled;
-    }
+		if (CachingServicePlugin.DEBUG)
+			System.out.println("< CachingServicePlugin.shouldRegister() " + enabled);
+		return enabled;
+	}
 
 }
