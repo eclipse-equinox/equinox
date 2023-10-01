401f89f0c68c684042defbb7f9b0a1d0fd371ce9
Torbjörn SVENSSON <torbjorn.svensson@st.com>
2023-10-01 23:07:24 +0200

    Format java files in o.e.equinox.weaving.caching.j9
    This was achieved by running:
    eclipse -consolelog -nosplash -application org.eclipse.jdt.core.JavaCodeFormatter \
      -config .settings/org.eclipse.jdt.core.prefs . -data `mktemp -d`
    
    Signed-off-by: Torbjörn SVENSSON <torbjorn.svensson@st.com>

--- bundles/org.eclipse.equinox.weaving.caching.j9/src/org/eclipse/equinox/weaving/internal/caching/j9/CachingServiceFactory.java
+++ bundles/org.eclipse.equinox.weaving.caching.j9/src/org/eclipse/equinox/weaving/internal/caching/j9/CachingServiceFactory.java
@@ -28,13 +28,12 @@ import org.osgi.framework.Bundle;
  */
 public class CachingServiceFactory implements ICachingServiceFactory {
 
-    /**
-     * @see org.eclipse.equinox.service.weaving.ICachingServiceFactory#createCachingService(java.lang.ClassLoader,
-     *      org.osgi.framework.Bundle, java.lang.String)
-     */
-    public ICachingService createCachingService(final ClassLoader classLoader,
-            final Bundle bundle, final String key) {
-        return new CachingService(classLoader, bundle, key);
-    }
+	/**
+	 * @see org.eclipse.equinox.service.weaving.ICachingServiceFactory#createCachingService(java.lang.ClassLoader,
+	 *      org.osgi.framework.Bundle, java.lang.String)
+	 */
+	public ICachingService createCachingService(final ClassLoader classLoader, final Bundle bundle, final String key) {
+		return new CachingService(classLoader, bundle, key);
+	}
 
 }
