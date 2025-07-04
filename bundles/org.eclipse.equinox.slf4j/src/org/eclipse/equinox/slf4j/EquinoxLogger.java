167d3fe739ca73c9da5f1b7488efdd71acd54f13
sougandhs <sougandh.s@ibm.com>
2025-07-04 12:36:54 +0530

    Update MarkerIgnoringBase with AbstractLogger
    This commit replaces org.slf4j.helpers.MarkerIgnoringBase with
    org.slf4j.helpers.AbstractLogger implementation
    
    Fixes : https://github.com/eclipse-equinox/equinox/issues/843

--- bundles/org.eclipse.equinox.slf4j/src/org/eclipse/equinox/slf4j/EquinoxLogger.java
+++ bundles/org.eclipse.equinox.slf4j/src/org/eclipse/equinox/slf4j/EquinoxLogger.java
@@ -10,14 +10,16 @@
  *
  * Contributors:
  *     Christoph Läubrich - initial API and implementation
+ *     IBM Corporation -  Update deprecated logger 
  *******************************************************************************/
 package org.eclipse.equinox.slf4j;
 
 import org.osgi.service.log.Logger;
-import org.slf4j.helpers.FormattingTuple;
+import org.slf4j.Marker;
+import org.slf4j.event.Level;
 import org.slf4j.helpers.MessageFormatter;
 
-class EquinoxLogger extends org.slf4j.helpers.MarkerIgnoringBase {
+class EquinoxLogger extends org.slf4j.helpers.AbstractLogger {
 
 	private static final long serialVersionUID = 1L;
 	private final Logger logger;
@@ -33,84 +35,8 @@ class EquinoxLogger extends org.slf4j.helpers.MarkerIgnoringBase {
 	}
 
 	@Override
-	public void trace(String msg) {
-		if (isTraceEnabled()) {
-			logger.trace(msg);
-		}
-	}
-
-	@Override
-	public void trace(String msg, Throwable t) {
-		if (isTraceEnabled()) {
-			logger.trace(msg, t);
-		}
-	}
-
-	@Override
-	public void trace(String format, Object arg1) {
-		if (isTraceEnabled()) {
-			FormattingTuple tp = MessageFormatter.format(format, arg1);
-			logger.trace(tp.getMessage(), tp.getThrowable());
-		}
-	}
-
-	@Override
-	public void trace(String format, Object arg1, Object arg2) {
-		if (isTraceEnabled()) {
-			FormattingTuple tp = MessageFormatter.format(format, arg1, arg2);
-			logger.trace(tp.getMessage(), tp.getThrowable());
-		}
-	}
-
-	@Override
-	public void trace(String format, Object... argArray) {
-		if (isTraceEnabled()) {
-			FormattingTuple tp = MessageFormatter.arrayFormat(format, argArray);
-			logger.trace(tp.getMessage(), tp.getThrowable());
-		}
-	}
-
-	@Override
 	public boolean isDebugEnabled() {
 		return logger != null && logger.isDebugEnabled();
-	}
-
-	@Override
-	public void debug(String msg) {
-		if (isDebugEnabled()) {
-			logger.debug(msg);
-		}
-	}
-
-	@Override
-	public void debug(String msg, Throwable t) {
-		if (isDebugEnabled()) {
-			logger.debug(msg, t);
-		}
-	}
-
-	@Override
-	public void debug(String format, Object arg1) {
-		if (isDebugEnabled()) {
-			FormattingTuple tp = MessageFormatter.format(format, arg1);
-			logger.debug(tp.getMessage(), tp.getThrowable());
-		}
-	}
-
-	@Override
-	public void debug(String format, Object arg1, Object arg2) {
-		if (isDebugEnabled()) {
-			FormattingTuple tp = MessageFormatter.format(format, arg1, arg2);
-			logger.debug(tp.getMessage(), tp.getThrowable());
-		}
-	}
-
-	@Override
-	public void debug(String format, Object... argArray) {
-		if (isDebugEnabled()) {
-			FormattingTuple tp = MessageFormatter.arrayFormat(format, argArray);
-			logger.debug(tp.getMessage(), tp.getThrowable());
-		}
 	}
 
 	@Override
@@ -119,126 +45,71 @@ class EquinoxLogger extends org.slf4j.helpers.MarkerIgnoringBase {
 	}
 
 	@Override
-	public void info(String msg) {
-		if (isInfoEnabled()) {
-			logger.info(msg);
-		}
-	}
-
-	@Override
-	public void info(String msg, Throwable t) {
-		if (isInfoEnabled()) {
-			logger.info(msg, t);
-		}
-	}
-
-	@Override
-	public void info(String format, Object arg1) {
-		if (isInfoEnabled()) {
-			FormattingTuple tp = MessageFormatter.format(format, arg1);
-			logger.info(tp.getMessage(), tp.getThrowable());
-		}
-	}
-
-	@Override
-	public void info(String format, Object arg1, Object arg2) {
-		if (isInfoEnabled()) {
-			FormattingTuple tp = MessageFormatter.format(format, arg1, arg2);
-			logger.info(tp.getMessage(), tp.getThrowable());
-		}
-	}
-
-	@Override
-	public void info(String format, Object... argArray) {
-		if (isInfoEnabled()) {
-			FormattingTuple tp = MessageFormatter.arrayFormat(format, argArray);
-			logger.info(tp.getMessage(), tp.getThrowable());
-		}
-	}
-
-	@Override
 	public boolean isWarnEnabled() {
 		return logger != null && logger.isWarnEnabled();
 	}
-
-	@Override
-	public void warn(String msg) {
-		if (isWarnEnabled()) {
-			logger.warn(msg);
-		}
-	}
-
-	@Override
-	public void warn(String msg, Throwable t) {
-		if (isWarnEnabled()) {
-			logger.warn(msg, t);
-		}
-	}
-
-	@Override
-	public void warn(String format, Object arg1) {
-		if (isWarnEnabled()) {
-			FormattingTuple tp = MessageFormatter.format(format, arg1);
-			logger.warn(tp.getMessage(), tp.getThrowable());
-		}
-	}
-
-	@Override
-	public void warn(String format, Object arg1, Object arg2) {
-		if (isWarnEnabled()) {
-			FormattingTuple tp = MessageFormatter.format(format, arg1, arg2);
-			logger.warn(tp.getMessage(), tp.getThrowable());
-		}
-	}
-
-	@Override
-	public void warn(String format, Object... argArray) {
-		if (isWarnEnabled()) {
-			FormattingTuple tp = MessageFormatter.arrayFormat(format, argArray);
-			logger.warn(tp.getMessage(), tp.getThrowable());
-		}
-	}
-
+	
 	@Override
 	public boolean isErrorEnabled() {
 		return logger != null && logger.isErrorEnabled();
 	}
-
+	
 	@Override
-	public void error(String msg) {
-		if (isErrorEnabled()) {
-			logger.error(msg);
-		}
+	public boolean isTraceEnabled(Marker marker) {
+		return isTraceEnabled();
 	}
 
 	@Override
-	public void error(String msg, Throwable t) {
-		if (isErrorEnabled()) {
-			logger.error(msg, t);
-		}
+	public boolean isDebugEnabled(Marker marker) {
+		return isDebugEnabled();
 	}
 
 	@Override
-	public void error(String format, Object arg1) {
-		if (isErrorEnabled()) {
-			FormattingTuple tp = MessageFormatter.format(format, arg1);
-			logger.error(tp.getMessage(), tp.getThrowable());
-		}
+	public boolean isInfoEnabled(Marker marker) {
+		return isInfoEnabled();
 	}
 
 	@Override
-	public void error(String format, Object arg1, Object arg2) {
-		if (isErrorEnabled()) {
-			FormattingTuple tp = MessageFormatter.format(format, arg1, arg2);
-			logger.error(tp.getMessage(), tp.getThrowable());
-		}
+	public boolean isWarnEnabled(Marker marker) {
+		return isWarnEnabled();
 	}
 
 	@Override
-	public void error(String format, Object... argArray) {
-		if (isErrorEnabled()) {
-			FormattingTuple tp = MessageFormatter.arrayFormat(format, argArray);
-			logger.error(tp.getMessage(), tp.getThrowable());
+	public boolean isErrorEnabled(Marker marker) {
+		return isErrorEnabled();
+	}
+
+	@Override
+	protected String getFullyQualifiedCallerName() {
+		return null;
+	}
+
+	@Override
+	protected void handleNormalizedLoggingCall(Level level, Marker marker, String messagePattern, Object[] arguments,
+			Throwable throwable) {	
+		if (logger == null) {
+			return;
+		}
+		
+		if(level == Level.TRACE && logger.isTraceEnabled()) {
+			String formattedMessage = MessageFormatter.basicArrayFormat(messagePattern, arguments);
+			logger.trace(formattedMessage,throwable);
+		}
+		if(level == Level.DEBUG && logger.isDebugEnabled()) {
+			String formattedMessage = MessageFormatter.basicArrayFormat(messagePattern, arguments);
+			logger.debug(formattedMessage,throwable);
+		}
+		if(level == Level.WARN && logger.isWarnEnabled()) {
+			String formattedMessage = MessageFormatter.basicArrayFormat(messagePattern, arguments);
+			logger.warn(formattedMessage,throwable);
+		}
+		if(level == Level.INFO && logger.isInfoEnabled()) {
+			String formattedMessage = MessageFormatter.basicArrayFormat(messagePattern, arguments);
+			logger.info(formattedMessage, throwable);
+		}
+		if(level == Level.ERROR && logger.isInfoEnabled()) {
+			String formattedMessage = MessageFormatter.basicArrayFormat(messagePattern, arguments);
+			logger.info(formattedMessage, throwable);
 		}
 	}
 
