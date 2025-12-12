3649666675316093d2d9f6dc86665f9701e3d8bf
Klare, Heiko <Heiko.Klare@vector.com>
2025-12-12 10:04:37 +0100

    Migrate LocationAreaSessionTest to JUnit 5
    This migrates the test case LocationAreaSessionTest to use the JUnit 5
    SessionTestExtension instead of the existing JUnit-3-based
    ConfigurationSessionTestSuite.

--- bundles/org.eclipse.osgi.tests/src/org/eclipse/osgi/tests/services/datalocation/AllTests.java
+++ bundles/org.eclipse.osgi.tests/src/org/eclipse/osgi/tests/services/datalocation/AllTests.java
@@ -13,11 +13,11 @@
  *******************************************************************************/
 package org.eclipse.osgi.tests.services.datalocation;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
 
-@RunWith(Suite.class)
-@Suite.SuiteClasses({ //
+@Suite
+@SelectClasses({ //
 		LocationAreaSessionTest.class, //
 		BasicLocationTests.class, //
 		SimpleTests.class, //
